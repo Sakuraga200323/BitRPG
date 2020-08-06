a3bad4a19a641ea054daed9c9cb75022cfab01a5
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-06 22:30:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -78,65 +78,67 @@ async def on_message(message):
         id = m_author.id
         print(id, id_list)
         if not id_list or not id in id_list:
-            await m_ch.send(
-                f"{m_author.mention}さんの冒険者登録を開始。"
-                "\n登録名を1分以内に送信してください。`next`と送信すると、ユーザー名がそのまま使用されます。\n`あとから設定し直すことが可能です。\n特殊文字非対応。`"
-            )
-            def check(m):
-                if not m.author.id == id:
-                    return 0
-                return 1
-            try:
-                msg = await client.wait_for("message", timeout=60, check=check)
-            except TimeoutError:
-                name = re.sub(r'[\x00-\x1f\x7f-\x9f]', '・', m_author.name)
-                await m_ch.send(f"時間切れです。ユーザー名『{name}』をそのまま登録します。")
-            else:
-                name = re.sub(r'[\x00-\x1f\x7f-\x9f]', '・', msg.content)
-                if name == "next":
+            flag = False
+            while flag == False:
+                await m_ch.send(
+                    f"{m_author.mention}さんの冒険者登録を開始。"
+                    "\n登録名を1分以内に送信してください。`next`と送信すると、ユーザー名がそのまま使用されます。\n`あとから設定し直すことが可能です。\n特殊文字非対応。`"
+                )
+                def check(m):
+                    if not m.author.id == id:
+                        return 0
+                    return 1
+                try:
+                    msg = await client.wait_for("message", timeout=60, check=check)
+                except TimeoutError:
                     name = re.sub(r'[\x00-\x1f\x7f-\x9f]', '・', m_author.name)
-                    await m_ch.send(f"ユーザー名『{name}』をそのまま登録します。")
+                    await m_ch.send(f"時間切れです。ユーザー名『{name}』をそのまま登録します。")
                 else:
-                    await m_ch.send(f"『{name}』で登録します。")
-            await m_ch.send("\n該当する性別の番号を20秒以内に送信してください。\n男性 -> 0\n女性 -> 1\n無記入 -> 3\n`半角全角は問いません。`")
-            def check2(m):
-                if not m.author.id == id:
-                    return 0
-                return 1
-            try:
-                msg2 = await client.wait_for("message", timeout=20, check=check2)
-                if not msg2.content in ("0", "1", "１", "０"):
-                    await m_ch.send("0か1の番号を送信してください")
-                    return
-            except TimeoutError:
-                sex = "無記入"
-                await m_ch.send(f"時間切れです。無記入として登録します。")
-            else:
-                sex = msg2.content
-                if sex in ("0", "０"):
-                    sex = "男性"
-                if sex in ("1", "１"):
-                    sex = "女性"
-                if sex in ("3", "３"):
+                    name = re.sub(r'[\x00-\x1f\x7f-\x9f]', '・', msg.content)
+                    if name == "next":
+                        name = re.sub(r'[\x00-\x1f\x7f-\x9f]', '・', m_author.name)
+                        await m_ch.send(f"ユーザー名『{name}』をそのまま登録します。")
+                    else:
+                        await m_ch.send(f"『{name}』で登録します。")
+                await m_ch.send("\n該当する性別の番号を20秒以内に送信してください。\n男性 -> 0\n女性 -> 1\n無記入 -> 3\n`半角全角は問いません。`")
+                def check2(m):
+                    if not m.author.id == id:
+                        return 0
+                    return 1
+                try:
+                    msg2 = await client.wait_for("message", timeout=20, check=check2)
+                except TimeoutError:
                     sex = "無記入"
-                await m_ch.send(f"『{sex}』で登録します。")
-            embed = discord.Embed(color = discord.Color.green())
-            embed.add_field(name = "Name", value = name)
-            embed.add_field(name = "Sex", value = sex)
-            await m_ch.send(embed=embed)
-            n = name
-            s = sex
-            i = '{"冒険者登録証明カード"}'
-            cmd = (
-                'INSERT INTO player_tb (name,sex,id,lv,max_hp, now_hp,max_mp, now_mp,str, def, agi,stp,str_stp, def_stp, agi_stp,all_exp, now_exp,money, items) '
-                + f"VALUES ('{n}', '{s}', {id}, 1, 10 ,10, 1, 1, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, " + f"'{i}');")
-            cur.execute(cmd)
-            await m_ch.send("登録完了しました。")
-            embed = discord.Embed(
-                description=f"{name}は`冒険者登録証明カード×1`を獲得した。",
-                color=discord.Color.green())
-            embed.set_thumbnail(url="https://media.discordapp.net/attachments/719855399733428244/740870252945997925/3ff89628eced0385.gif")
-            await m_ch.send(embed=embed)
+                    await m_ch.send(f"時間切れです。無記入として登録します。")
+                else:
+                    sex = msg2.content
+                    if not sex in ("0", "1", "１", "０", "2","２"):
+                        await m_ch.send("0か1の番号を送信してください。")
+                            continue
+                    if sex in ("0", "０"):
+                        sex = "男性"
+                    if sex in ("1", "１"):
+                        sex = "女性"
+                    if sex in ("2", "２"):
+                        sex = "無記入"
+                    await m_ch.send(f"『{sex}』で登録します。")
+                embed = discord.Embed(color = discord.Color.green())
+                embed.add_field(name = "Name", value = name)
+                embed.add_field(name = "Sex", value = sex)
+                await m_ch.send(embed=embed)
+                n = name
+                s = sex
+                i = '{"冒険者登録証明カード"}'
+                cmd = (
+                    'INSERT INTO player_tb (name,sex,id,lv,max_hp, now_hp,max_mp, now_mp,str, def, agi,stp,str_stp, def_stp, agi_stp,all_exp, now_exp,money, items) '
+                    + f"VALUES ('{n}', '{s}', {id}, 1, 10 ,10, 1, 1, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, " + f"'{i}');")
+                cur.execute(cmd)
+                await m_ch.send("登録完了しました。")
+                embed = discord.Embed(
+                    description=f"{name}は`冒険者登録証明カード×1`を獲得した。",
+                    color=discord.Color.green())
+                embed.set_thumbnail(url="https://media.discordapp.net/attachments/719855399733428244/740870252945997925/3ff89628eced0385.gif")
+                await m_ch.send(embed=embed)
 
     if m_ctt.startswith("SystemCall\n"):
         m_ctt = m_ctt.split("SystemCall\n")[1]
