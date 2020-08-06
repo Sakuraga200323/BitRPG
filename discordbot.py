012a6dce6bbd2fdd14e20a19d75b1b08a8a06823
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-06 17:46:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -77,7 +77,7 @@ async def on_message(message):
         id = m_author.id
         print(id, id_list)
         if not id in id_list:
-            cur.execute(f'''INSERT INTO player_tb (
+            cur.execute('''INSERT INTO player_tb (
                 name,
                 id,lv,
                 max_hp, now_hp,
@@ -86,8 +86,9 @@ async def on_message(message):
                 stp,
                 str_stp, def_stp, agi_stp,
                 all_exp, now_exp,
-                money, items) VALUES (
-                {re.sub(r'[\x00-\x1f\x7f-\x9f]', '', m_author.name)},
+                money, items) VALUES ('''
+                + f"{re.sub(r'[\x00-\x1f\x7f-\x9f]', '', m_author.name)}" 
+                + ''',
                 m_author.id, 1 ,
                 10 , 10,
                 1 ,1 ,
