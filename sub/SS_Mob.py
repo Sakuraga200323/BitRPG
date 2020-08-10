966790e8bd948caed597aea606ef876cc567814a
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-11 02:12:03 +0900

    Add files via upload

---
+++ sub/SS_Mob.py
@@ -0,0 +1,5 @@
+set = {
+    "Texa":"https://media.discordapp.net/attachments/719489738939301938/719686581174140969/5e804b95f8d7cffb7090ad95733a3d09.gif",
+    "時空の狭間":"https://media.discordapp.net/attachments/719489738939301938/719686743280058408/fdf1cd7d2454971cedd30509e02a4648.gif?width=479&height=671"
+
+}
---
+++ sub/S_Mob.py
@@ -0,0 +1,8 @@
+set = {
+    "Ajna":"http://folce.zatunen.com/m77.png",
+    "RogueniteElite":"http://folce.zatunen.com/m19.png",
+    "ValkyrieElite":"http://folce.zatunen.com/m52.png",
+    "LizardGoblinElite":"http://folce.zatunen.com/m37.png",
+    "DarkKnight":"http://folce.zatunen.com/m36.png",
+    "Odin":"http://folce.zatunen.com/m62.png"
+}
