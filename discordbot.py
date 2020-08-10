1dd77911913d319a48f1b98d1aa7f5899560212e
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-10 13:37:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -151,17 +151,14 @@ async def on_message(message):
                     'INSERT INTO player_tb (name,sex,id,lv,max_hp, now_hp,max_mp, now_mp,str, def, agi,stp,str_stp, def_stp, agi_stp,all_exp, now_exp,money, items) '
                     + f"VALUES ('{n}', '{s}', {id}, 1, 10 ,10, 1, 1, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, " + f"'{i}');"
                 )
-                try:
-                    cur.execute(cmd)
-                except:
-                    await m_ch.send("冒険者登録に失敗しました。\n再度お試しください。")
-                else:
-                    await m_ch.send("登録完了しました。")
-                    embed = discord.Embed(
-                        description=f"{name}は`冒険者登録証明カード×1`を獲得した。",
-                        color=discord.Color.green())
-                    embed.set_thumbnail(url="https://media.discordapp.net/attachments/719855399733428244/740870252945997925/3ff89628eced0385.gif")
-                    await m_ch.send(embed=embed)
+                cur.execute(cmd)
+                await m_ch.send("冒険者登録に失敗しました。\n再度お試しください。")
+                await m_ch.send("登録完了しました。")
+                embed = discord.Embed(
+                    description=f"{name}は`冒険者登録証明カード×1`を獲得した。",
+                    color=discord.Color.green())
+                embed.set_thumbnail(url="https://media.discordapp.net/attachments/719855399733428244/740870252945997925/3ff89628eced0385.gif")
+                await m_ch.send(embed=embed)
         if  m_ch.id in sub.box.cmd_ch:
             sub.box.cmd_ch.remove(m_ch.id)
             
