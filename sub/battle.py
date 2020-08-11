b54b235f0384bd4d0e4d7ec725b19f0d5c07c154
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-11 17:43:30 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -113,6 +113,7 @@ def cbt_proc(user,ch):
             log1_1 += "強ダメージ！"; X = 1.5
         dmg1 = round(X * dmg1)
         m_data["now_hp"] -= dmg1
+        pg.execute(f"update mob_tb set now_hp = {m_data['now_hp']} where id = {m_data['id'];")
         log1_1 += str(dmg1)
         log1_1 += f'\n{m_data["name"]}のHP[{m_data["now_hp"]}/{m_data["max_hp"]}]'
         if m_data["now_hp"] <= 0:
@@ -129,6 +130,7 @@ def cbt_proc(user,ch):
                 log2_1 += "強ダメージ！"; X = 1.5
             dmg2 = round(X * dmg2)
             p_data["now_hp"] -= dmg2
+            pg.execute(f"update player_tb set now_hp = {p_data['now_hp']} where id = {p_data['id'];")
             log2_1 += str(dmg2)
             log2_1 += f'\n{p_data["name"]}のHP[{p_data["now_hp"]}/{p_data["max_hp"]}]'
             if p_data["now_hp"] <= 0:
