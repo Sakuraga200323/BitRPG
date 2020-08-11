d1b248b2c3883444f226547c37c9721d5e413196
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-11 12:37:26 +0900

    Update calc.py

--- sub/calc.py
+++ sub/calc.py
@@ -1,6 +1,11 @@
-def dmg_calc(STR, DEF):
+import random
+
+def randint(a,b):
+    return random.randint(a,b)
+
+def dmg(STR,DEF):
     value = random.randint(80,120)
-    dmg = (( value * STR ) - ( DEF * 50 )) / 100
+    dmg = ((value*STR) - (DEF*50))/100
     if dmg <= 0:
         dmg = 1
     return dmg
