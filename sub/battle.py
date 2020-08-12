1af24a888c75cfa9ef404ac12d854fca31553abd
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-13 03:30:15 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -229,7 +229,7 @@ def cbt_proc(user,ch):
             except:
                 loop.create.tasks(ch.send(f"【注意】{i_data['name']} の戦闘離脱処理が正常に作動しなかった可能性が発生。"))
             i_data = pg.fetchdict(f"select * from player_tb where id = {i}")[0]
-        if luck >= 1:
+        if luck >= 1000:
             a = p_data["items"]
             a.append('魔石')
             print(f'{a}')
