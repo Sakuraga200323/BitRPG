30e74903dc1bc090225005c7b8846ce5e8567092
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-12 22:14:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -320,7 +320,7 @@ async def on_message(message):
                     else:
                         if not m_author.id in sub.box.cbt_user:
                             p_hp = pg.fetch(f"select max_hp from player_tb where id = {m_author.id};")[0]
-                            pg.execute(f"update player_tb set now_hp = {p_hp}")
+                            pg.execute(f"update player_tb set now_hp = {p_hp[0]}")
                             await m_ch.send(f"HPを回復しました。")
                         await m_ch.send(f"『{m_ch.name}』で戦闘は実行されていません。")
                     
