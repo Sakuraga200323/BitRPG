a7b105432c2550e96c31c5255718bf30b6181f11
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-12 13:18:54 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -227,7 +227,7 @@ def cbt_proc(user,ch):
             em.set_thumbnail(url = "https://media.discordapp.net/attachments/719855399733428244/720967442439864370/maseki.png")
             em_list.append(em)
         embed = discord.Embed(title = "Result",description = desc,color = discord.Color.green())
-        pg.execute(f"update into player_tb set cbt_ch_id = NULL where cbt_ch_id = {ch.id}")
+        pg.execute(f"update player_tb set cbt_ch_id = NULL where cbt_ch_id = {ch.id}")
         if ch.id in sub.box.cbt_ch:
             del sub.box.cbt_ch[ch.id]
     log1_2 = f"```diff\n{log1_1}```"
