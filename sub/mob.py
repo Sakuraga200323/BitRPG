b37a0d6a8b7d75fd6f6182ea4a97902b95553988
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-12 23:12:38 +0900

    Update mob.py

--- sub/mob.py
+++ sub/mob.py
@@ -97,3 +97,5 @@ def appear(m_data):
     ch = client.get_channel(int(m_data['id']))
     if ch:
         loop.create_task(ch).send(embed = embed)
+    else:
+        print(f"{m_data['id']}のチャンネルが見つかりません")
