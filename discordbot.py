b0a02de70afa2551a961720dc387a1f343bb5af3
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-12 13:20:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -280,13 +280,13 @@ async def on_message(message):
                             return
                         for i in sub.box.cbt_ch[ch.id]:
                             p_hp = pg.fetch(f"select max_hp from player_tb where id = {user.id};")[0]
-                            pg.execute(f"update into player_tb set now_hp = {p_hp}")
+                            pg.execute(f"update player_tb set now_hp = {p_hp}")
                             if not i.id in sub.box.cbt_user:
                                 return
                             del sub.box.cbt_user[i.id]
                         m_data = pg.fetchdict(f"select * from mob_tb where id = {ch.id};")[0]
                         await m_ch.send(f"{m_data['name']}(Lv:{m_data['lv']}) との戦闘が解除されました。")
-                        pg.execute(f"update into player_tb set now_hp = {m_daga['max_hp']}")
+                        pg.execute(f"update player_tb set now_hp = {m_daga['max_hp']}")
                         rank = "Normal"
                         color = discord.Color.blue()
                         if m_data["lv"] % 1000 == 0:
