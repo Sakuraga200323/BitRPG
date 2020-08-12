c22b7329a71c364d59f04dbdfede69e75c482711
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-12 12:13:37 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -103,7 +103,7 @@ def cbt_proc(user,ch):
     log2_1 = ""
     luck = random.randint(0, 100)
     if p_data["agi"] >= m_data["agi"]:
-        log1_1 += f'+ {p_data["name"]}の攻撃！'
+        log1_1 += f'+ {p_data["name"]} の攻撃！'
         temp = "ダメージ", ; X = 1
         if luck >= 95:
             temp = "極ダメージ！"; X = 3
@@ -118,10 +118,10 @@ def cbt_proc(user,ch):
         log1_1 += f"の{temp}"
         log1_1 += f'\n{m_data["name"]}のHP[{m_data["now_hp"]}/{m_data["max_hp"]}]'
         if m_data["now_hp"] <= 0:
-            log2_1 = f'{m_data["name"]}を倒した！！'
+            log2_1 = f'{m_data["name"]} を倒した！！'
             m_data["lv"] += 1
         else:
-            log2_1 += f'+ {m_data["name"]}の攻撃！'
+            log2_1 += f'+ {m_data["name"]} の攻撃！'
             X = 1
             if luck >= 95:
                 log2_1 += "極ダメージ！"; X = 3
@@ -135,7 +135,44 @@ def cbt_proc(user,ch):
             log2_1 += str(dmg2)
             log2_1 += f'\n{p_data["name"]}のHP[{p_data["now_hp"]}/{p_data["max_hp"]}]'
             if p_data["now_hp"] <= 0:
-                log2_1 += f'{p_data["name"]}はやられてしまった！！'
+                log2_1 += f'{p_data["name"]} はやられてしまった！！'
+
+
+    else:
+        log1_1 += f'+ {m_data["name"]} の攻撃！'
+        temp = "ダメージ", ; X = 1
+        if luck >= 95:
+            temp = "極ダメージ！"; X = 3
+        elif luck >= 90:
+            temp += "超ダメージ！"; X = 2
+        elif luck >= 85:
+            temp += "強ダメージ！"; X = 1.5
+        dmg2 = round(X * dmg2)
+        p_data["now_hp"] -= dmg2
+        pg.execute(f"update player_tb set now_hp = {p_data['now_hp']} where id = {p_data['id']};")
+        log1_1 += str(dmg2)
+        log1_1 += f"の{temp}"
+        log1_1 += f'\n{p_data["name"]} のHP[{p_data["now_hp"]}/{p_data["max_hp"]}]'
+        if p_data["now_hp"] <= 0:
+            log2_1 = f'{p_data["name"]} はやられてしまった！！'
+            m_data["lv"] += 1
+        else:
+            log2_1 += f'+ {p_data["name"]} の攻撃！'
+            X = 1
+            if luck >= 95:
+                log2_1 += "極ダメージ！"; X = 3
+            elif luck >= 90:
+                log2_1 += "超ダメージ！"; X = 2
+            elif luck >= 85:
+                log2_1 += "強ダメージ！"; X = 1.5
+            dmg1 = round(X * dmg1)
+            M_data["now_hp"] -= dmg1
+            pg.execute(f"update mob_tb set now_hp = {m_data['now_hp']} where id = {m_data['id']};")
+            log2_1 += str(dmg1)
+            log2_1 += f'\n{m_data["name"]} のHP[{m_data["now_hp"]}/{m_data["max_hp"]}]'
+            if p_data["now_hp"] <= 0:
+                log2_1 += f'{m_data["name"]} を倒した！！'
+
     embed = None
     em = None
     if first_moblv < m_data["lv"]:
@@ -150,7 +187,7 @@ def cbt_proc(user,ch):
                 i_data["lv"] += 1
                 if p_data["lv"] % 10 == 0:
                     p_data["stp"] += 50
-            desc += f'\n{i_data["name"]}が`{get_exp}Exp`獲得'
+            desc += f'\n{i_data["name"]} が`{get_exp}Exp`獲得'
             if i_data["lv"] > be_lv:
                 i_data["str"] = 10*(i_data["lv"] + 1) + i_data["str_stp"]
                 i_data["def"] = 10*(i_data["lv"] + 1) + i_data["def_stp"]
@@ -158,11 +195,7 @@ def cbt_proc(user,ch):
                 i_data["max_hp"] = 10*(i_data["lv"] + 1)
                 i_data["now_hp"] = i_data["max_hp"]
                 i_data["now_mp"] = i_data["lv"]
-                desc += f"\n{i_data['name']}はLvUP `{be_lv}->{i_data['lv']}`"
-            try:
-                del box.cbt_user[i.id]
-            except Exception as e:
-                pass
+                desc += f"\n{i_data['name']} はLvUP `{be_lv}->{i_data['lv']}`"
             pg.execute(
                 f'''update player_tb set
                     lv = {i_data["lv"]},
@@ -178,16 +211,23 @@ def cbt_proc(user,ch):
                     now_exp = {i_data["now_exp"]},
                     money = {i_data["money"] + (round(m_data["lv"]/len(box.cbt_ch[ch.id])))} where id = {i.id};'''
             )
+            try:
+                if i.id in sub.box.cbt_user:
+                    del sub.box.cbt_user[user.id]
+            exception:
+                await ch.send(f"【注意】{i_data['name']} の戦闘離脱処理が正常に作動しなかった可能性が発生。")
         if luck >= 99:
-            a = p_data["items"].split("]")[0] + ",'魔石',]"
+            a = p_data["items"].append("魔石")
             pg.execute(
                 f'''update player_tb set items = "{a}";'''
             )
             em = discord.Embed(
-                description = f"{player.mention}が魔石を発見！")
+                description = f"{p_data['name']} が魔石を発見！")
             em.set_thumbnail(url = "https://media.discordapp.net/attachments/719855399733428244/720967442439864370/maseki.png")
             em_list.append(em)
         embed = discord.Embed(title = "Result",description = desc,color = discord.Color.green())
+        if ch.id in sub.box.cbt_ch:
+            del sub.box.cbt_ch[ch.id]
     log1_2 = f"```diff\n{log1_1}```"
     log2_2 = f"```diff\n{log2_1}```"
     battle_log = f"{log1_2}{log2_2}"
