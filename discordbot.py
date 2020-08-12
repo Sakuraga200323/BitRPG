e79e384ac2a98cb41cb5336697132650f0dccf82
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-12 22:18:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -295,7 +295,7 @@ async def on_message(message):
                             pg.execute(f"update player_tb set now_hp = {p_hp[0]}")
                             if not i in sub.box.cbt_user:
                                 return
-                            del sub.box.cbt_user[i.id]
+                            del sub.box.cbt_user[i]
                         m_data = pg.fetchdict(f"select * from mob_tb where id = {m_ch.id};")[0]
                         await m_ch.send(f"{m_data['name']}(Lv:{m_data['lv']}) との戦闘が解除されました。")
                         pg.execute(f"update mob_tb set now_hp = {m_data['max_hp']}")
