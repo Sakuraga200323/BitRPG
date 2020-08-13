be2b32e1d277e59697a895892a7eec2291c5b84c
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-08-13 20:24:30 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -200,14 +200,14 @@ def cbt_proc(user,ch):
             while i_data["now_exp"] > i_data["lv"]:
                 i_data["now_exp"] -= i_data["lv"]
                 i_data["lv"] += 1
-                if i_data["lv"] % 10 == 0:
-                    i_data["stp"] += 50
             desc += f'\n{i_data["name"]} が`{get_exp}Exp`獲得'
             if i_data["lv"] > be_lv:
-                i_data["str"] += 10
-                i_data["def"] += 10
-                i_data["agi"] += 10
-                i_data["max_hp"] += 10*(i_data["lv"] + 1)
+                num_temp = i_data["lv"] - be_lv
+                i_data["str"] += 10*num_temp 
+                i_data["def"] += 10*num_temp 
+                i_data["agi"] += 10*num_temp
+                i_data["stp"] += 10*num_temp 
+                i_data["max_hp"] += 10**num_temp 
                 i_data["now_hp"] = i_data["max_hp"]
                 i_data["now_mp"] = i_data["lv"]
                 desc += f"\n{i_data['name']} はLvUP `{be_lv}->{i_data['lv']}`"
