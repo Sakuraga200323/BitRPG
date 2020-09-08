0f0b66bd737785b51b29b17bfcb394c81e79fa3c
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-09-08 09:35:16 +0900

    Update item.py

--- sub/item.py
+++ sub/item.py
@@ -47,7 +47,7 @@ loop = asyncio.get_event_loop()
 def open(client, ch, user):
     items_dtd = pg.fetchdict("select items from player_tb;")[0]
     text = ""
-    for item, num = zip(items_dtd.keys(), items_dtd.items()):
+    for item, num in zip(items_dtd.keys(), items_dtd.items()):
         text += f"{item}(`{num}`)\n"
     embed = discord.Embed(
         title="Player Inventory Bord",
