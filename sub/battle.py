d2eb311d124aa0646bccdd8042393455575caa64
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-09-08 10:56:33 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -260,7 +260,7 @@ def cbt_proc(user,ch):
             ITEMS = ["HP回復薬","MP回復薬","ドーピング薬","魔石"]
             item = random.choice(ITEMS)
             item_num = pg.fetchdict(f"SELECT items->'{item}' as item_num FROM player_tb;")[0]["item_num"]
-            get_num = random.randint(0,3)
+            get_num = random.randint(1,6)
             item_num += get_num
             pg.execute(f"update player_tb set items = items::jsonb||json_build_object('{item}', {item_num})::jsonb;")
             item_em = discord.Embed(
