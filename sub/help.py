d5789c55e200920ddaa152cba1391b34baf95e1c
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-09-12 12:52:09 +0900

    Update help.py

--- sub/help.py
+++ sub/help.py
@@ -27,22 +27,21 @@ async def help(client, ch, user):
             return
         if msg.channel.id != ch.id:
             return
-        if not msg.content in target_list:
-            rate_result = {}
-            for i in target_list:
-                rate = difflib.SequenceMatcher(None, msg.content, i).ratio()
-                rate_result[i] = rate
-            yosou = inverse_lookup(rate_result, rate_result.values().max())
-            await ch.send(f"{msg.content}はHelpに登録されていません。コマンドの場合は省略形で入れている可能性があります。原形で探してみてください。\n`help, status, attack, result, item, point, rank, str, def, agi, stp, exp, player, mob, money`\n※独断と勝手な偏見で予想しましたが、もしかして探しているのは`{yosou}`ではないですか？")
-            return
     try:
-        remsg = await client.wait_for("message", timeout=20, check=await check)
+        remsg = await client.wait_for("message", timeout=20, check=check)
     except asyncio.TimeoutError:
         await ch.send("20秒経過、受付を終了します。")
         sended_em.set_author(text="処理終了済み")
     else:
         target = remsg.content
-        
+        if not target in target_list:
+            rate_result = {}
+            for i in target_list:
+                rate = difflib.SequenceMatcher(None, target, i).ratio()
+                rate_result[i] = rate
+            yosou = inverse_lookup(rate_result, rate_result.values().max())
+            await ch.send(f"{target}はHelpに登録されていません。コマンドの場合は省略形で入れている可能性があります。原形で探してみてください。\n`help, status, attack, result, item, point, rank, str, def, agi, stp, exp, player, mob, money`\n※独断と勝手な偏見で予想しましたが、もしかして探しているのは`{yosou}`ではないですか？")
+            return
         
          
         
