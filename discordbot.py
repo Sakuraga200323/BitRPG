98f2123be029ad8cb3fee604974a765038c334b5
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-09-12 14:11:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -299,7 +299,7 @@ async def on_message(message):
                         rate = difflib.SequenceMatcher(None, m_ctt.split(" ")[0], i).ratio()
                         rate_result[i] = rate
                     yosou = inverse_lookup( rate_result, max( list( rate_result.values() ) ) )
-                    if not max( list( rate_result.values() ) ) ==0
+                    if not max( list( rate_result.values() ) ) == 0:
                         await m_ch.send(
                             f"`{m_ctt.split(' ')[0]}`というコマンドはありません。\nもしかして`{yosou}`では？")
                     return
