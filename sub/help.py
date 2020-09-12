bf8f5ecc9a2b41b000fd5778da9be7acdcf8fd48
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-09-12 12:57:26 +0900

    Update help.py

--- sub/help.py
+++ sub/help.py
@@ -39,7 +39,7 @@ async def help(client, ch, user):
             for i in target_list:
                 rate = difflib.SequenceMatcher(None, target, i).ratio()
                 rate_result[i] = rate
-            yosou = inverse_lookup(rate_result, rate_result.values().max())
+            yosou = inverse_lookup(rate_result, list(rate_result.values()).max())
             await ch.send(f"{target}はHelpに登録されていません。コマンドの場合は省略形で入れている可能性があります。原形で探してみてください。\n`help, status, attack, result, item, point, rank, str, def, agi, stp, exp, player, mob, money`\n※独断と勝手な偏見で予想しましたが、もしかして探しているのは`{yosou}`ではないですか？")
             return
         
