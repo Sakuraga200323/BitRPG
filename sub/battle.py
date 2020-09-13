23ce8ce80b3d1dc730c833e082d2c1bfa27fb240
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-09-13 13:40:46 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -265,7 +265,7 @@ async def cbt_proc(user,ch):
 
         if m_data["rank"] == "UltraRare":
             item_num = pg.fetchdict(f"SELECT items->'魔石' as item_num FROM player_tb;")[0]["item_num"]
-            item_num += １００
+            item_num += 100
             pg.execute(f"update player_tb set items = items::jsonb||json_build_object('魔石', {item_num})::jsonb where id = {p_data['id']};")
             item_em = discord.Embed(description = f"{p_data['name']} は魔石×{get_num}を獲得し！")
         if random.random() >= 0.95:
