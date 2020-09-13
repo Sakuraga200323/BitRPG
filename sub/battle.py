cc336c47ce2c92badcbcac00fd2baf0ae18eeb79
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-09-13 18:43:25 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -222,13 +222,14 @@ async def cbt_proc(user,ch):
             while i_data["now_exp"] > i_data["lv"] and i_data["lv"] <= i_data["max_lv"]:
                 i_data["now_exp"] -= i_data["lv"]
                 i_data["lv"] += 1
+                if i_data["lv"] % 10 == 0:
+                    i_data["stp"] += 50 
             desc += f'\n{i_data["name"]} が`{get_exp}Exp`獲得'
             if i_data["lv"] > be_lv:
                 num_temp = i_data["lv"] - be_lv
                 i_data["str"] = 10*(i_data["lv"] + 1) + i_data["str_stp"]
                 i_data["def"] = 10*(i_data["lv"] + 1)  + i_data["def_stp"]
                 i_data["agi"] = 10*(i_data["lv"] + 1) + i_data["agi_stp"]
-                i_data["stp"] += 10 
                 i_data["max_hp"] = 10*(i_data["lv"] + 1) 
                 i_data["now_hp"] = i_data["max_hp"]
                 i_data["now_mp"] = i_data["lv"]
