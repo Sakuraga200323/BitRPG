69f09b96abc0f2519e420dd76a7e25db70d514a8
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-09-22 15:33:56 +0900

    Update status.py

--- sub/status.py
+++ sub/status.py
@@ -49,7 +49,7 @@ async def send_bord(client, user, ch):
     embed = discord.Embed(title = "Player Status Board")
     embed.add_field(name = f"Player", value = f"{p_data['name']}({user.mention})", inline = False)
     embed.add_field(name = f"Sex", value = f"{p_data['sex']}", inline = False)
-    embed.add_field(name = f"Lv (Level)", value = f"*{p_data['lv']} / {p_data['max_lv']}*"
+    embed.add_field(name = f"Lv (Level)", value = f"*{p_data['lv']} / {p_data['max_lv']}*")
     embed.add_field(name = f"HP (HitPoint)", value = f"*{p_data['now_hp']} / {p_data['max_hp']}*")
     embed.add_field(name = f"MP (MagicPoint)", value = f"*{p_data['now_mp']} / {p_data['max_mp']}*")
     embed.add_field(name = f"STR (Strength)", value = f"*{p_data['str'] + p_data['str_p']}*\n`(+{p_data['str_p']})`")
