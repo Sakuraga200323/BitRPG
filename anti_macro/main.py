c4d9e5cfb7af660b4ab35fc67b23ad923a774ef1
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-10-02 19:30:27 +0900

    Update main.py

--- anti_macro/main.py
+++ anti_macro/main.py
@@ -1,28 +1,38 @@
 import cv2
-import numpy as np
-
-def rotate(img, angle, scale):
-    """
-    画像を回転（反転）させる
-    [in]  img:   回転させる画像
-    [in]  angle: 回転させる角度
-    [in]  scale: 拡大率
-    [out] 回転させた画像
-    """
-
-    size = img.shape[:2]
-    mat = cv2.getRotationMatrix2D((size[0] // 2, size[1] // 2), angle, scale)
-    return cv2.warpAffine(img, mat, size, flags=cv2.INTER_CUBIC)
+from paste import paste
+import io
+import tempfile
+import requests
+from matplotlib import pyplot as plt
 
 
-def rotateR(img, level=[-10, 10], scale=1.2):
-    """
-    ランダムに画像を回転させる
-    [in]  img:   回転させる画像
-    [in]  level: 回転させる角度の範囲
-    [out] 回転させた画像
-    [out] 回転させた角度
-    """
+import discord
+client = None
 
-    angle = np.random.randint(level[0], level[1])
-    return rotate(img, angle, scale), angle
+def get_img(client):
+    global client;client = client
+    channel = client.get_channel(761521730035646464)
+    channel2 = client.get_channel(761522350248165486)
+    imgs_front = [ i.attachments[0].url channel.history()]
+    imgs_back = dict(
+        zip(
+            [ i.attachments[0].url for i in channel2.history()],[ i.content for i in channel2.history()])
+    front_url = random.choice(imgs_front)
+    back_url = random.choice(imgs_front)
+    num =  imgs_front[back_url]
+    # 画像をリクエストする
+    def get_from_url(url):
+        res = requests.get(url)
+        img = None
+        # Tempfileを作成して即読み込む
+        with tempfile.NamedTemporaryFile(dir='./') as fp:
+            fp.write(res.content)
+            fp.file.seek(0)
+            img = cv2.imread(fp.name)
+        return img
+    img = paste(
+        get_from_url(front_url),# 前景
+        get_from_url(back_url),# 背景
+        False, False # 縁フラグ、回転・移動フラグ
+    )
+    return img, num
