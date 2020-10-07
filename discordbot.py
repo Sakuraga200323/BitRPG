71cdf860c22660b41e8a8ef2814a78bdd7cbbb55
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-10-07 15:32:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -316,47 +316,6 @@ async def on_message(message):
 
 
         try:
-            try:
-                check = False # random.random() >= 0.0
-            finally:
-                if check:
-                    macro_checking.append(m_author.id)
-                    img, num = await anti_macro.get_img(client)
-                    cv2.imwrite('anti_macro/num_img/temp.jpg', img)
-                    await m_ch.send(f'{m_author.mention}さんのマクロチェックです。\n以下の画像に書かれている数字を20秒以内に**半角**で送信してください。', file=discord.File(fp="anti_macro/num_img/temp.jpg"))
-                    def check(m):
-                        if not m.author.id == id or m.channel.id != m_ch.id:
-                            return 0
-                        if not m.content in ['0','1','2','3','4','5','6','7','8','9']:
-                            return 0
-                        return 1
-                    try:
-                        answer = await client.wait_for('message', timeout=20, check=check)
-                    except asyncio.TimeoutError:
-                        if num and not m_author.id in doubt_count:
-                            doubt_count[m_authir.id] = 0
-                        doubt_count[m_author.id] += 1
-                        
-                        await m_ch.send(f'無回答!!　不正カウント+1(現在{doubt_count[m_author.id]})')
-                    else:
-                        if answer.content == num:
-                            await m_ch.send(f'正解!! 報酬として現レベル×10の経験値を配布しました。')
-                            pg.execute(f'update player_tb set now_exp = now_exp + (lv*10) where id = {m_author.id};')
-                            retunr
-                        if doubt_count[m_authir.id] >= 5:
-                            await m_ch.send(f'不正カウントが規定量に達しました。貴方のプレイヤーデータを即座に終了します。')
-                            pg.execute(f'delete from player_tb where id = {m_author.id};')
-                        if not num != str(answer.content):
-                            if num and not m_author.id in doubt_count:
-                                doubt_count[m_authir.id] = 0
-                            doubt_count[m_author.id] += 1
-                            await m_ch.send(f'不正解!! 不正カウント+1(現在{doubt_count[m_author.id]})')
-                            if doubt_count[m_authir.id] >= 5:
-                                await m_ch.send(f'不正カウントが規定量に達しました。貴方のプレイヤーデータを即座に終了します。')
-                                pg.execute(f'delete from player_tb where id = {m_author.id};')
-                                
-                        
-
 
             cmd_list = ["^^help","^^st","^^status","^^point","^^attack","^^atk","^^rank","^^item","^^reset","^^re"]
 
@@ -523,19 +482,19 @@ async def on_message(message):
             
             
 
-        if m_ctt.stratswith("--"):
+        if m_ctt.startswith("--"):
             if not m_author.id in clr_lv4 and not m_author.id in clr_lv5 :
                 await m_ch.send("**貴方のクリアランスはLv4未満です。プロトコル[SimpleSystemCall]の実行にはLv4以上のクリアランスが必要です。**")
                 return
             if m_ctt == "--test anti_macro":
                 try:
-                    check = random.random() >= 0.00
+                    check = False # random.random() >= 0.0
                 finally:
                     if check:
                         macro_checking.append(m_author.id)
                         img, num = await anti_macro.get_img(client)
                         cv2.imwrite('anti_macro/num_img/temp.jpg', img)
-                        await m_ch.send(f'{m_author.mention}さんのマクロチェックです。\n以下の画像に書かれている数字を20秒以内に**半角**で送信してください。\n※実際にアカウントが消去されることはありません(多分)', file=discord.File(fp="anti_macro/num_img/temp.jpg"))
+                        await m_ch.send(f'{m_author.mention}さんのマクロチェックです。\n以下の画像に書かれている数字を20秒以内に**半角**で送信してください。', file=discord.File(fp="anti_macro/num_img/temp.jpg"))
                         def check(m):
                             if not m.author.id == id or m.channel.id != m_ch.id:
                                 return 0
@@ -550,8 +509,6 @@ async def on_message(message):
                             doubt_count[m_author.id] += 1
 
                             await m_ch.send(f'無回答!!　不正カウント+1(現在{doubt_count[m_author.id]})')
-                            if doubt_count[m_authir.id] >= 5:
-                                await m_ch.send(f'不正カウントが規定量に達しました。貴方のプレイヤーデータを即座に終了します。')
                         else:
                             if answer.content == num:
                                 await m_ch.send(f'正解!! 報酬として現レベル×10の経験値を配布しました。')
@@ -559,7 +516,7 @@ async def on_message(message):
                                 retunr
                             if doubt_count[m_authir.id] >= 5:
                                 await m_ch.send(f'不正カウントが規定量に達しました。貴方のプレイヤーデータを即座に終了します。')
-                                return
+                                pg.execute(f'delete from player_tb where id = {m_author.id};')
                             if not num != str(answer.content):
                                 if num and not m_author.id in doubt_count:
                                     doubt_count[m_authir.id] = 0
@@ -567,7 +524,8 @@ async def on_message(message):
                                 await m_ch.send(f'不正解!! 不正カウント+1(現在{doubt_count[m_author.id]})')
                                 if doubt_count[m_authir.id] >= 5:
                                     await m_ch.send(f'不正カウントが規定量に達しました。貴方のプレイヤーデータを即座に終了します。')
-                                
+                                    pg.execute(f'delete from player_tb where id = {m_author.id};')
+
                         
     if m_ctt == "SystemCall":
         m_ctt = m_ctt.split("SystemCall")[1].strip("\n")
