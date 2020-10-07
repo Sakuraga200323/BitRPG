e5ac320385b388008298b97c0aae17ce3864788f
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-10-07 17:21:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -493,56 +493,59 @@ async def on_message(message):
                     check = random.random() >= 0.0
                 finally:
                     if check:
-                        flag = await m_ch.send("デデドン！！")
-                        await asyncio.sleep(1)
-                        check_id = flag.id
-                        macro_checking.append(m_author.id)
-                        img, num = await anti_macro.get_img(client)
-                        cv2.imwrite('anti_macro/num_img/temp.png', img)
-                        check_em = discord.Embed(
-                            title = "マクロ検知ぃいい！！(迫真)",
-                            description=f'{m_author.mention}さんのマクロチェックです。\n以下の画像に書かれている数字を20秒以内に**半角**で送信してください。\n※`CheckID『{check_id}』`')
-                        check_em.set_image(url="attachment://temp.png")
-                        await m_ch.send(embed=check_em,file=discord.File(fp="anti_macro/num_img/temp.png"))
-                        if num and not m_author.id in doubt_count:
-                            doubt_count[m_author.id] = 0
-                        def check(m):
-                            if not m.author.id == m_author.id or m.channel.id != m_ch.id:
-                                return 0
-                            if not m.content in ['0','1','2','3','4','5','6','7','8','9']:
-                                return 0
-                            return 1
-                        try:
-                            answer = await client.wait_for('message', timeout=20, check=check)
-                        except asyncio.TimeoutError:
-                            doubt_count[m_author.id] += 1
-                            temp = None
-                            await m_ch.send(f'無回答!!　不正カウント+1(現在{doubt_count[m_author.id]})')
-                        else:
-                            temp = answer.content
-                            if answer.content == num:
-                                await m_ch.send(f'正解!! 報酬として現レベル×10の経験値を配布しました。')
-                                pg.execute(f'update player_tb set now_exp = now_exp + (lv*10) where id = {m_author.id};')
-                                retun
-                            if not num != str(answer.content):
+                        check_flag = True
+                        while check_flag = True:
+                            flag = await m_ch.send("デデドン！！")
+                            await asyncio.sleep(1)
+                            check_id = flag.id
+                            macro_checking.append(m_author.id)
+                            img, num = await anti_macro.get_img(client)
+                            cv2.imwrite('anti_macro/num_img/temp.png', img)
+                            check_em = discord.Embed(
+                                title = "マクロ検知ぃいい！！(迫真)",
+                                description=f'{m_author.mention}さんのマクロチェックです。\n以下の画像に書かれている数字を20秒以内に**半角**で送信してください。\n※`CheckID『{check_id}』`')
+                            check_em.set_image(url="attachment://temp.png")
+                            await m_ch.send(embed=check_em,file=discord.File(fp="anti_macro/num_img/temp.png"))
+                            if num and not m_author.id in doubt_count:
+                                doubt_count[m_author.id] = 0
+                            def check(m):
+                                if not m.author.id == m_author.id or m.channel.id != m_ch.id:
+                                    return 0
+                                if not m.content in ['0','1','2','3','4','5','6','7','8','9']:
+                                    return 0
+                                return 1
+                            try:
+                                answer = await client.wait_for('message', timeout=20, check=check)
+                            except asyncio.TimeoutError:
                                 doubt_count[m_author.id] += 1
-                                await m_ch.send(f'不正解!! 不正カウント+1(現在{doubt_count[m_author.id]})')
-                        print(f"MacroCheck：({m_author.id}) TrueAnswer[{num}], UsersAnswer[{temp}]")
-                        result = True
-                        pg = Postgres(dsn)
-                        P_list = pg.fetch(f"select * from player_tb where id = {m_author.id};")
-                        if doubt_count[m_author.id] >= 5:
-                            result = False
-                            doubt_count[m_author.id] = 0
-                            await m_ch.send(f'不正カウントが規定量に達しました。貴方のプレイヤーデータを即座に終了します。')
-                            pg.execute(f"delete from player_tb where id = {m_author.id};")
-                            await m_ch.send(f"「この画像は誰が見てもわからんやろ！？」等の異議申し立てがある場合は`^^claim {check_id}`と送信してください。運営人の検知画像肉眼チェックの上然るべき対応をさせていただきます。")
-                        embed=discord.Embed(title="マクロ検知ログ", color=0x37ff00)
-                        embed.add_field(name="CheckID", value=check_id, inline=False)
-                        embed.add_field(name="Result", value=result, inline=False)
-                        embed.add_field(name="UserData", value=P_list, inline=False)
-                        embed.set_image(url="attachment://temp.png")
-                        await client.get_channel(763299968353304626).send(embed=embed, file=discord.File(fp="anti_macro/num_img/temp.png"))
+                                temp = None
+                                await m_ch.send(f'無回答!!　不正カウント+1(現在{doubt_count[m_author.id]})')
+                            else:
+                                temp = answer.content
+                                if answer.content == num:
+                                    await m_ch.send(f'正解!! 報酬として現レベル×10の経験値を配布しました。')
+                                    pg.execute(f'update player_tb set now_exp = now_exp + (lv*10) where id = {m_author.id};')
+                                    check_flag = Flase
+                                    retun
+                                if not num != str(answer.content):
+                                    doubt_count[m_author.id] += 1
+                                    await m_ch.send(f'不正解!! 不正カウント+1(現在{doubt_count[m_author.id]})')
+                            print(f"MacroCheck：({m_author.id}) TrueAnswer[{num}], UsersAnswer[{temp}]")
+                            result = True
+                            pg = Postgres(dsn)
+                            P_list = pg.fetch(f"select * from player_tb where id = {m_author.id};")
+                            if doubt_count[m_author.id] >= 5:
+                                result = False
+                                doubt_count[m_author.id] = 0
+                                await m_ch.send(f'不正カウントが規定量に達しました。貴方のプレイヤーデータを即座に終了します。')
+                                pg.execute(f"delete from player_tb where id = {m_author.id};")
+                                await m_ch.send(f"「この画像は誰が見てもわからんやろ！？」等の異議申し立てがある場合は`^^claim {check_id}`と送信してください。運営人の検知画像肉眼チェックの上然るべき対応をさせていただきます。")
+                            embed=discord.Embed(title="マクロ検知ログ", color=0x37ff00)
+                            embed.add_field(name="CheckID", value=check_id, inline=False)
+                            embed.add_field(name="Result", value=result, inline=False)
+                            embed.add_field(name="UserData", value=P_list, inline=False)
+                            embed.set_image(url="attachment://temp.png")
+                            await client.get_channel(763299968353304626).send(embed=embed, file=discord.File(fp="anti_macro/num_img/temp.png"))
                         
 
                         
