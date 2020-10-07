677f486970e92891fe26604b29a18a7e8fbc8470
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-10-07 18:09:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -128,7 +128,7 @@ async def loop():
     MEM = psutil.virtual_memory().percent
     if client.get_channel(761571389345759232).name=='true':
         sub_msg = "現在開発作業中につき停止中￤"
-    await client.change_presence(activity=discord.Game(name=f"^^url￤{len(client.guilds)}Server"))
+    await client.change_presence(activity=discord.Game(name=f"^^url￤{len(client.guilds)}server"))
 
 
 @client.event
