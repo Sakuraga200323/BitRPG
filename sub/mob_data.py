d4ae858f05e28a8a91f38b732f68e4b860504d81
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-11-23 18:07:25 +0900

    Create mob_data.py

---
+++ sub/mob_data.py
@@ -0,0 +1,40 @@
+normal = {
+    1:("Skeleton","https://media.discordapp.net/attachments/719489738939301938/719490176627507230/hone.png"),
+    2:("Roguenite","https://cdn.discordapp.com/attachments/719489738939301938/719490298845069322/heisi.png"),
+    3:("Golem","https://media.discordapp.net/attachments/719489738939301938/719490326066102292/iwao.png"),
+    4:("LizardGoblin","https://media.discordapp.net/attachments/719489738939301938/719490348933709874/guremrin.png"),
+    5:("Skeleton","https://media.discordapp.net/attachments/719489738939301938/719490411198152775/hone2.png"),
+    6:("Orc","https://media.discordapp.net/attachments/719489738939301938/719490421964668958/gobb.png"),
+    7:("Sludge","https://media.discordapp.net/attachments/719489738939301938/719493729550860339/slimred.png"),
+    8:("GoblinSoldier","https://media.discordapp.net/attachments/719489738939301938/719493795850485760/red.png"),
+    9:("Mummy","https://media.discordapp.net/attachments/719489738939301938/719494134330556457/mummy.png"),
+    10:("Valkyrie","https://media.discordapp.net/attachments/719489738939301938/719494265759072286/tougisya.png"),
+    11:("Lorg","http://folce.zatunen.com/m85.png"),
+    12:("Manticore","http://folce.zatunen.com/m78.png"),
+    13:("Succubus","http://folce.zatunen.com/m67.png"),
+    14:("Theaf","http://folce.zatunen.com/m17.png"),
+    15:("Bicorn","http://folce.zatunen.com/m13.png"),
+    16:("Kerberos","http://folce.zatunen.com/illust/m15.png"),
+    17:("Phelios","http://folce.zatunen.com/m11.png"),
+}
+
+elite = {
+    1:("Ajna","http://folce.zatunen.com/m77.png"),
+    2:("RogueniteElite","http://folce.zatunen.com/m19.png"),
+    3:("ValkyrieElite","http://folce.zatunen.com/m52.png),
+    4:("LizardGoblinElite","http://folce.zatunen.com/m37.png"),
+    5:("DarkKnight","http://folce.zatunen.com/m36.png"),
+    6:("Odin","http://folce.zatunen.com/m62.png")
+}
+  
+worldend = {
+    1:("Texa","https://media.discordapp.net/attachments/719489738939301938/719686581174140969/5e804b95f8d7cffb7090ad95733a3d09.gif"),
+    2:("時空の狭間","https://media.discordapp.net/attachments/719489738939301938/719686743280058408/fdf1cd7d2454971cedd30509e02a4648.gif?width=479&height=671")
+}
+
+rare = {
+}
+
+ultrarare = {
+  "1":("古月",https://media.discordapp.net/attachments/719489738939301938/750412647203209266/download20200903025142.png?width=585&height=585)
+}
