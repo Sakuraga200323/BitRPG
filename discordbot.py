e595ad032ea5058ed651065f4f1721c00fb9cfbd
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-11-24 18:10:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -263,12 +263,14 @@ async def on_message(message):
     m_guild = message.guild
     m_author = message.author
 
+    '''
     if m_author.id == 302050872383242240:
         if message.embeds:
             if message.embeds[0].description:
                 desc = message.embeds[0].description
                 if "表示順を" in desc:
                     id = desc.split("<@")[1].split(">")[0]
+    '''
 
                     
     if m_ctt.startswith("^^") and not m_author.id in macro_checking and not m_author.bot:
