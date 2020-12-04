35a1998f5a065c76c261ae93ba9e6175ce83894e
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-04 17:34:52 +0900

    Update status.py

--- sub/status.py
+++ sub/status.py
@@ -67,7 +67,7 @@ async def send_bord(client, user, ch):
         r = f"REM`：{bar(p_data.now_stp(), p_data.STP())}`"
         embed.add_field(name=f"StatusPointBalance (Sum:{p_data.now_stp()})", value=f"{s}\n{d}\n{a}\n{r}", inline=False)
     exp_bar = '<:1_:784323561052569642>'*int( p_data.now_exp() / p_data.lv() *20)
-    expbar2 = (20 - int( p_data.now_exp() / p_data.lv() * 20)) * '<:0_:784323507110150144>'
+    exp_bar2 = (20 - int( p_data.now_exp() / p_data.lv() * 20)) * '<:0_:784323507110150144>'
     print(exp_bar)
     embed.add_field(name = f"Experience", value=(
           f"*{p_data.EXP()}*"
