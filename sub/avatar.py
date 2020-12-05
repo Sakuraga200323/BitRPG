d7459797ebfad55f1cfbdf7b31be6b69b49a4c61
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-05 17:46:43 +0900

    Update avatar.py

--- sub/avatar.py
+++ sub/avatar.py
@@ -86,7 +86,7 @@ class Player:
         self.client = client
         self.dtd = self.pg.fetchdict(f"select * from player_tb where id = {self.user.id};")[0]
         print(list(self.dtd.values())[1:12])
-        self.lv_, self.max_exp_, self.now_exp_, self.now_stp_, self.str_p_, self.def_p_, self.agi_p_, self.magic_class_, self.kill_count_, self.item_, self.money_ = list(self.dtd.values())[1:12]
+        self.lv_, self.max_lv_, self.max_exp_, self.now_exp_, self.now_stp_, self.str_p_, self.def_p_, self.agi_p_, self.magic_class_, self.kill_count_, self.item_, self.money_ = list(self.dtd.values())[1:13]
         self.max_hp = self.now_hp = self.lv_ * 100 + 10
         self.max_mp = self.now_mp = self.lv_ * 10
         self.battle_ch = None
