cee3bdf57d42a47b2071fc649ff30210787df12f
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-05 18:43:28 +0900

    Update avatar.py

--- sub/avatar.py
+++ sub/avatar.py
@@ -237,7 +237,7 @@ class Player:
         lvup_count = 0
         self.now_exp()
         use_exp = 0
-        while self.now_exp() >= lv and self.max_lv() >= lv:
+        while self.now_exp() >= lv and self.max_lv() >= self.lv():
             lvup_count += 1
             use_exp += self.lv_
             self.lv(1)
