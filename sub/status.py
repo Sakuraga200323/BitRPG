eda1ca7f7f25c94b2217d41d8fcde82d27132b8b
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-05 22:53:26 +0900

    Update status.py

--- sub/status.py
+++ sub/status.py
@@ -18,32 +18,13 @@ JST = timezone(timedelta(hours=+9), 'JST')
 
 dsn = os.environ.get('DATABASE_URL')
 
-class Postgres:
-    def __init__(self, dsn):
-        self.conn = psycopg2.connect(dsn)
-        self.conn.autocommit = True
-        self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-    def execute(self, sql):
-        self.cur.execute(sql)
-
-    def fetch(self, sql):
-        self.cur.execute(sql)
-        return self.cur.fetchall()
-
-    def fetchdict(self, sql):
-        self.cur.execute (sql)
-        results = self.cur.fetchall()
-        dict_result = []
-        for row in results:
-            dict_result.append(dict(row))
-        return dict_result
 
 def my_round(val, digit=0):
     p = 10 ** digit
     return (val * p * 2 + 1) // 2 / p
 
-pg = Postgres(dsn)
+pg = None
 
 async def open_status(client, user, ch):
     if not user.id in box.players:
