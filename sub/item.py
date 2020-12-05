267c3d07cba7427822426ee7586bc64676d165d7
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-05 22:52:12 +0900

    Update item.py

--- sub/item.py
+++ sub/item.py
@@ -18,26 +18,6 @@ JST = timezone(timedelta(hours=+9), 'JST')
 
 dsn = os.environ.get('DATABASE_URL')
 
-class Postgres:
-    def __init__(self, dsn):
-        self.conn = psycopg2.connect(dsn)
-        self.conn.autocommit = True
-        self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
-    def execute(self, sql):
-        self.cur.execute(sql)
-
-    def fetch(self, sql):
-        self.cur.execute(sql)
-        return self.cur.fetchall()
-
-    def fetchdict(self, sql):
-        self.cur.execute (sql)
-        results = self.cur.fetchall()
-        dict_result = []
-        for row in results:
-            dict_result.append(dict(row))
-        return dict_result
 
 ITEMS = (
     "HP回復薬",
@@ -57,7 +37,7 @@ ITEMS_IMG_URL = {
     "魔石":"https://media.discordapp.net/attachments/719855399733428244/757449362652790885/maseki.png"
 }
     
-pg = Postgres(dsn)
+pg = None
 
 
 async def open(client, ch, user):
