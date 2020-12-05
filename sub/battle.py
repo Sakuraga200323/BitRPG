40557d8145c4b9a12e04ba70fa9664d351f6382a
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-05 22:50:46 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -18,26 +18,6 @@ from sub import box, calc, avatar
 JST = timezone(timedelta(hours=+9), 'JST')
 dsn = os.environ.get('DATABASE_URL')
 
-class Postgres:
-    def __init__(self, dsn):
-        self.conn = psycopg2.connect(dsn)
-        self.conn.autocommit = True
-        self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
-    def execute(self, sql):
-        self.cur.execute(sql)
-
-    def fetch(self, sql):
-        self.cur.execute(sql)
-        return self.cur.fetchall()
-
-    def fetchdict(self, sql):
-        self.cur.execute (sql)
-        results = self.cur.fetchall()
-        dict_result = []
-        for row in results:
-            dict_result.append(dict(row))
-        return dict_result
 
 
 token = os.environ.get('TOKEN')
@@ -60,7 +40,7 @@ getmagic_list = [
     "006|LifeConversion"
 ]
 
-pg = Postgres(dsn)
+pg = None
 
 async def cbt_proc(client, user, ch):
     if not user.id in box.players:
