16e353caf21af34a171bab7527ff501fa2ba8954
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-10 01:59:26 +0900

    Update help.py

--- sub/help.py
+++ sub/help.py
@@ -16,11 +16,11 @@ async def help(client, ch, user):
     embed = discord.Embed(
         title="BitRPBHelpURL",
         description=("各URLから公式ヘルプページにジャンプ出来ます。青字でないところはページが未完成です。"
-            +"[BitRPG内の用語](https://github.com/Sakuraga200323/BitRPG/blob/master/help_page/1)BitRPG%E5%86%85%E3%81%AE%E7%94%A8%E8%AA%9E.md)"
-            +"[各コマンドの使い方](https://github.com/Sakuraga200323/BitRPG/blob/master/help_page/2)%E5%90%84%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%81%AE%E4%BD%BF%E3%81%84%E6%96%B9.md)"
-            +"戦闘システム"
-            +"[アイテム](https://github.com/Sakuraga200323/BitRPG/blob/master/help_page/4)%E3%82%A2%E3%82%A4%E3%83%86%E3%83%A0.md)"
-            +"魔法領域"
-            +"未登録(要望があれば増えます)"
+            +"\n[BitRPG内の用語](https://github.com/Sakuraga200323/BitRPG/blob/master/help_page/1)BitRPG%E5%86%85%E3%81%AE%E7%94%A8%E8%AA%9E.md)"
+            +"\n[各コマンドの使い方](https://github.com/Sakuraga200323/BitRPG/blob/master/help_page/2)%E5%90%84%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%81%AE%E4%BD%BF%E3%81%84%E6%96%B9.md)"
+            +"\n戦闘システム"
+            +"\n[アイテム](https://github.com/Sakuraga200323/BitRPG/blob/master/help_page/4)%E3%82%A2%E3%82%A4%E3%83%86%E3%83%A0.md)"
+            +"\n魔法領域"
+            +"\n未登録(要望があれば増えます)"
     ))
     await ch.send(embed=embed)
