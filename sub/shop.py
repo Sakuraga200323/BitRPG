50a58dfed4af0a6a68400394921a5bfab4dd7a35
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-11 18:41:52 +0900

    Update shop.py

--- sub/shop.py
+++ sub/shop.py
@@ -115,8 +115,8 @@ async def shop(client, ch, user):
                 if not result:
                     await ch.send('ちゃんと注文して')
                     return
-                item_id = item_name = item_num = None
-                item_id, item_name, item_num = int(result.group(1))+6, items_name[item_id], int(result.group(2))
+                item_id, item_num = int(result.group(1))+6, int(result.group(2))
+                item_name = items_name[item_id]
                 item_dtd = pg.fetchdict(f"select item from player_tb where id = {user.id};")[0]["item"]
                 material_dict = { 7:( (5,1,),(6,1,) ), 8:( (4,1),(5,1),(7,1) ) }
                 cost_dict = {7:750,8:1000}
