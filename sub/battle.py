69f3d513f3b94c42da6f6aac7e7e66d02e8e464a
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-12 13:27:06 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -19,7 +19,6 @@ JST = timezone(timedelta(hours=+9), 'JST')
 dsn = os.environ.get('DATABASE_URL')
 
 
-
 token = os.environ.get('TOKEN')
 
 admin_list = [
@@ -67,135 +66,66 @@ async def cbt_proc(client, user, ch):
         print("box.playersに存在しないPlayer.idを取得")
         if not user.id in [i["id"] for i in pg.fetchdict(f"select id from player_tb;")]:
             box.players[user.id] = avatar.Player(client, user.id)
-            print(f"Playerデータ挿入(battle.py>cbt_proc)： {box.players[user.id].user}")
-    player = box.players[user.id]
-    mob = box.mobs[ch.id]
+            print(f"Playerデータ挿入(battle.py->cbt_proc)： {box.players[user.id].user}")
+    player,mob = box.players[user.id],box.mobs[ch.id]
     if not player.battle_start(ch.id):
         channel = client.get_channel(player.battle_ch)
         if channel:
             await ch.send(f"<@{user.id}> は現在『<@{channel.id}>』で戦闘中です。")
             return
+        await ch.send(f"<@{user.id}> が認識できないチャンネルで戦闘中。データの上書きを行ないます。")
+        player.battle_end()
+        if player.battle_start(ch.id):
+            await ch.send(f"上書き完了")
         else:
-            await ch.send(f"<@{user.id}> のデータに存在しないチャンネルIDが記載されていました。データの上書きを行ないます。")
-            player.battle_end()
-            if player.battle_start(ch.id):
-                await ch.send(f"上書き完了")
-            else:
-                await ch.send(f"上書き失敗、戦闘に参加できていません。")
-                return
+            await ch.send(f"上書き失敗、戦闘に参加できていません。")
+            return
 
     if player.now_hp <= 0:
         await ch.send(f"<@{user.id}> は既に死亡しています。")
         return
     mob.player_join(user.id)
 
-
     # モンスターとの戦闘で使うダメージ、運の計算およびログの設定 #
-    dmg1 = calc.dmg(player.STR(), mob.defe())
-    dmg2 = calc.dmg(mob.str(), player.DEFE())
-    if mob.name == "古月":
-        dmg2 *= 0.75
-        dmg2 = int(dmg2*2)
-    log1_1 = ""
-    log2_1 = ""
-    luck = random.randint(0, 100)
-    luck2 = random.randint(0, 100)
+    dmg1,dmg2 = calc.dmg(player.STR(), mob.defe()),calc.dmg(mob.str(), player.DEFE())
+    dmg2 = int(dmg2*1.45) if mob.name=="古月" else dmg2
+    log1_1 = log2_1 = ""
 
-    # HPゲージの取得
+    # HPゲージの作成
     def hp_gauge(now, max):
-        gauge_num = int((now/max)*20)
-        return (gauge_num*"/") + ((20 - gauge_num)*" ")
+        return  "-"*20 if now<=0 else (int((now/max)*20)*"/")+((20-int((now/max)*20))*" ")
+
+    a,b = random.random(),random.random()
+    t,x = ("極",5) if a>=0.95 else ("超",2) if a>=0.9 else ("強",1.5) if a>=0.85 ("",1)
+    t2,x2 = ("極",5) if b>=0.95 else ("超",2) if b>=0.9 else ("強",1.5) if b>=0.85 ("",1)
+    t +=　"ダメージ！"; t2 +=　"ダメージ！"
 
     # 戦闘処理（Player先手） #
     if player.AGI() >= mob.agi():
         log1_1 += f'+ {player.user}の攻撃->'
-        t = "ダメージ"
-        X = 1
-        if random.random() >= 0.95:
-            t = "極ダメージ！"
-            X = 3
-        elif random.random() >= 0.9:
-            t = "超ダメージ！"
-            X = 2
-        elif random.random() >= 0.85:
-            t = "強ダメージ！"
-            X = 1.5
-        dmg1 = round(X * dmg1)
-        if dmg1 != 0:
-            log1_1 += str(dmg1)
-            log1_1 += f"の{t}"
-        else:
-            if dmg1 == 0:
-                log1_1 += "しかし当たらなかった"
-        log1_1 += f'\n{mob.name} のHP[{mob.cut_hp(dmg1)}/{mob.max_hp}]'
-        log1_1 += f"\n[{hp_gauge(mob.now_hp, mob.max_hp)}]"
-        if mob.now_hp <= 0:
-            log2_1 = f'{mob.name}を倒した！！'
-        else:
-            log2_1 += f'- {mob.name}の攻撃->'
-            X = 1
-            t2 = "ダメージ"
-            if random.random() >= 0.95:
-                t2 = "極ダメージ！"; X = 3
-            elif random.random() >= 0.9:
-                t2 = "超ダメージ！"; X = 2
-            elif random.random() >= 85:
-                t2 = "強ダメージ！"; X = 1.5
-            dmg2 = round(X * dmg2)
-            if dmg2 != 0:
-                log2_1 += str(dmg2)
-                log2_1 += f"の{t2}"
-            elif dmg2 == 0:
-                log2_1 += "しかし当たらなかった！"
-            log2_1 += f'\n{user} のHP[{player.cut_hp(dmg2)}/{player.max_hp}]'
-            log2_1 += f"\n[{hp_gauge(player.now_hp, player.max_hp)}]"
-            if player.now_hp <= 0:
-                log2_1 += f'{user}はやられてしまった！！'
-
+        dmg1 = round(x * dmg1)
+        log1_1 += f"{str(dmg1)}の{t}" if dmg1!=0 else "しかし当たらなかった…"
+        log1_1 += f'\n{mob.name} のHP[{mob.cut_hp(dmg1)}/{mob.max_hp}]\n[{hp_gauge(mob.now_hp, mob.max_hp)}]
+        log2_1 += f'{mob.name}を倒した！！' if mob.now_hp<=0 else f'- {mob.name}の攻撃->'
+        if not mob.now_hp <= 0:
+            dmg2 = round(x2 * dmg2)
+            log2_1 += f"{str(dmg2)}の{t2}" if dmg2!=0 else "しかし当たらなかった…"
+            log2_1 += f'\n{user} のHP[{player.cut_hp(dmg2)}/{player.max_hp}]\n[{hp_gauge(player.now_hp, player.max_hp)}]'
 
     # 戦闘処理（Player後手） #
     else:
         log1_1 += f'- {mob.name}の攻撃->'
-        t = "ダメージ" ; X = 1
-        if random.random() >= 0.95:
-            t = "極ダメージ！"; X = 3
-        elif random.random() >= 0.9:
-            t = "超ダメージ！"; X = 2
-        elif random.random() >= 85:
-            t = "強ダメージ！"; X = 1.5
-        dmg2 = round(X * dmg2)
-        if not dmg2 == 0:
-            log1_1 += str(dmg2)
-            log1_1 += f"の{t}"
-        elif dmg2 == 0:
-            log1_1 += "しかし当たらなかった！"
-        log1_1 += f'\n{user}のHP[{player.cut_hp(dmg2)}/{player.max_hp}]'
-        log1_1 += f"\n[{hp_gauge(player.now_hp, player.max_hp)}]"
-        if player.now_hp <= 0:
-            log2_1 = f'\n{user}はやられてしまった！！'
-        else:
-            log2_1 += f'+ {user}の攻撃->'
-            t2 = "ダメージ" ; X = 1
-            if random.random() >= 0.95:
-                t2 = "極ダメージ！"; X = 3
-            elif random.random() >= 0.9:
-                t2 = "超ダメージ！"; X = 2
-            elif random.random() >= 85:
-                t2 = "強ダメージ！"; X = 1.5
-            dmg1 = round(X * dmg1)
-            if not dmg1 == 0:
-                log2_1 += str(dmg1)
-                log2_1 += f"の{t2}"
-            elif dmg1 == 0:
-                log2_1 += "しかし当たらなかった！"
-            log2_1 += f'\n{mob.name}のHP[{mob.cut_hp(dmg1)}/{mob.max_hp}]'
-            log2_1 += f"\n[{hp_gauge(mob.now_hp, mob.max_hp)}]"
-            if mob.now_hp <= 0:
-                log2_1 += f'\n{mob.name}を倒した！！'
+        dmg2 = round(x * dmg2)
+        log1_1 += f"{str(dmg2)}の{t}" if dmg1!=0 else "しかし当たらなかった…"
+        log1_1 += f'\n{user}のHP[{player.cut_hp(dmg2)}/{player.max_hp}]\n[{hp_gauge(player.now_hp, player.max_hp)}]
+        log2_1 += f'{user}はやられてしまった！！' if player.now_hp<=0 else f'- {user}の攻撃->'
+        if not player.now_hp<=0
+            dmg1 = round(x2 * dmg1)
+            if not player.now_hp <= 0:
+            log2_1 += f"{str(dmg1)}の{t2}" if dmg1!=0 else "しかし当たらなかった…"
+            log2_1 += f'\n{mob.name}のHP[{mob.cut_hp(dmg1)}/{mob.max_hp}]\n[{hp_gauge(mob.now_hp, mob.max_hp)}]'
 
-    log1_2 = f"```diff\n{log1_1}```"
-    log2_2 = f"```diff\n{log2_1}```"
-    battle_log = f"{log1_2}{log2_2}"
+    battle_log = f"```diff\n{log1_1}``````diff\n{log2_1}```"
 
 
     # バフのターンとかの確認 #
@@ -212,13 +142,13 @@ async def cbt_proc(client, user, ch):
 
 
     embed = em = item_em = spawn_embed = None
+
     if mob.now_hp <= 0:
         desc = ""
         now = datetime.now(JST).strftime("%H:%M")
         if  now in ['23:18']:
             get_exp *= 16
             await ch.send("????『幸運を。死したものより祝福を。』")
-
         exp, money = mob.exp()
         money = int(money/len(mob.battle_players))
         print("戦闘参加しているPlayer: ",mob.battle_players)
@@ -229,80 +159,61 @@ async def cbt_proc(client, user, ch):
             p.money(money)
             desc += f"<@{p_id}> 経験値+{exp} "
             desc += f"お金+{money} "
-            if up_lv > 0:
-                desc += f"\nLvUP {p.lv()-up_lv} -> {p.lv()}"
-            desc += "\nドロップアイテム："
-
+            if up_lv > 0: desc += f"\nLvUP {p.lv()-up_lv} -> {p.lv()}"
+            desc += "\nドロップアイテム： "
+            def get_item_sub(c=client, u=user, item_id, item_num):
+                status.get_item(c, u, item_id, item_num)
+                desc += f"{item_emoji_a[item_id]}×{item_num} "
             if random.random() <= 0.05:
                 # HPポーション
-                item_id = 2
-                item_num = random.randint(3, 6)
-                status.get_item(client, user, item_id, item_num)
-                desc += f"{item_emoji_a[item_id]}×{item_num} "
+                item_id,item_num = 2,random.randint(3, 6)
+                get_item_sub(item_id, item_num)
             if random.random() <= 0.05:
                 # MPポーション
-                item_id = 3
-                item_num = random.randint(3, 6)
-                status.get_item(client, user, item_id, item_num)
-                desc += f"{item_emoji_a[item_id]}×{item_num} "
+                item_id,item_num = 3,random.randint(3, 6)
+                get_item_sub(item_id, item_num)
             if True:
                 # 魂の焔
-                item_id = 4
-                status.get_item(client, user, item_id, 1)
-                desc += f"{item_emoji_a[item_id]}×1 "
+                item_id,item_num = 4,1
+                get_item_sub(item_id, item_num)
             if random.random() <= 0.5 and mob.name in ("Golem",):
                 # 砥石
-                item_id = 5
-                item_num = random.randint(1, 2)
-                status.get_item(client, user, item_id, item_num)
-                desc += f"{item_emoji_a[item_id]}×{item_num} "
+                item_id,item_num = 5,random.randint(1, 2)
+                get_item_sub(item_id, item_num)
             if random.random() <= 0.03:
                 # 魔石
-                item_id = 6
-                item_num = random.randint(3, 6)
-                status.get_item(client, user, item_id, item_num)
-                desc += f"{item_emoji_a[item_id]}×{item_num} "
-
-
-        if random.random() >= 0.99:
+                item_id,item_num = 6,random.randint(3, 6)
+                get_item_sub(item_id, item_num)
+        if random.random() <= 0.01:
             player.now_stp(mob.lv())
             em = discord.Embed(description=f"<@{user.id}> は{mob.lv()}のSTPを獲得した！")
-
-
-        embed = discord.Embed(title="Result",description = desc,color = discord.Color.green())
+        embed = discord.Embed(title="Result",description=desc,color=discord.Color.green())
         mob.lv(1)
         spawn_embed = mob.battle_end()
     
-    
     await ch.send(content=battle_log,embed = embed)
-    if em:
-        await ch.send(embed=em)
-    if item_em:
-        await ch.send(embed=item_em)
-    if spawn_embed:
-        await ch.send(embed=spawn_embed)
+    if em: await ch.send(embed=em)
+    if item_em: await ch.send(embed=item_em)
+    if spawn_embed: await ch.send(embed=spawn_embed)
 
-
-
+        
+# 戦闘から離脱 #
 async def reset(client, user, ch):
     if not user or not ch:
-        await ch.send("バグでプレイヤーもしくはチャンネルが見つかりません。")
+        await ch.send("プレイヤーもしくはチャンネルが見つかりません。")
         rerturn
     if not user.id in box.players:
         box.players[user.id] = avatar.Player(client, user.id)
         print(f"Playerデータ挿入： {box.players[user.id].user}")
         return
-    player = box.players[user.id]
-    mob = box.mobs[ch.id]
-
+    player,mob = box.players[user.id],box.mobs[ch.id]
     if not player.battle_ch:
         player.now_hp = player.max_hp
         await ch.send(f"HPを全回復しました。")
         return
-
     now_ch = client.get_channel(player.battle_ch)
     if player.battle_ch != ch.id:
-        await ch.send(f"<@{player.user.id}> は<@{now_ch.id}> で戦闘中です。")
+        await ch.send(f"<@{player.user.id}> は『<@{now_ch.id}>』で戦闘中です。")
         return
     mob.battle_end()
     await ch.send(embed = mob.spawn())
