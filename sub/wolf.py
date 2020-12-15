233a7e96c832e2b24321f47f551e128d1730fab6
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-15 23:56:04 +0900

    Update wolf.py

--- sub/wolf.py
+++ sub/wolf.py
@@ -40,7 +40,7 @@ async def magci_1(player,mob):
     await battle.battle_start(player,mob)
     dmg1 = calc.dmg(player.STR()*2,mob.defe())
     dmg2 = calc.dmg(mob.str(),player.DEFE()*0.8)
-    if player.now_mp <= 50:
+    if player.now_mp < 50:
         dmg = 0
     p_text = m_text = ""
     p_text += f"{player.user}の『BeeRay』->{dmg1}ダメージ！"
