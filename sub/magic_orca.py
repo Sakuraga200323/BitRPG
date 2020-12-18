f192a6623c3414c10b0cb26e2c2b8fac43d0df83
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-18 17:56:38 +0900

    Update magic_orca.py

--- sub/magic_orca.py
+++ sub/magic_orca.py
@@ -80,11 +80,11 @@ async def magic_5(player,mob):
 async def open_magic(user,ch):
     player = box.players[user.id]
     magic_em = discord.Embed(title="Player Magic Board",description="各魔法の数値は熟練度による補正を加算済みです。")
-    magic_em.add_field(name="`1.`StunRain",value=f"必要熟練度： **0**\nSTR**{80+(player.magic_lv()/1000)}**%の攻撃魔法 **50**%で敵を**3**ターンStun状態にする ",inline=False)
-    magic_em.add_field(name="`2.`-",value=f"`必要熟練度： 500\n未実装`",inline=False)
-    magic_em.add_field(name="`3.`-",value=f"`必要熟練度： 1000\n未実装`",inline=False)
-    magic_em.add_field(name="`4.`-+",value=f"`必要熟練度： 2000\n未実装`",inline=False)
-    magic_em.add_field(name="`5.`-",value=f"`必要熟練度： 4000\n未実装`",inline=False)
+    magic_em.add_field(name="`1.`StunRain",value=f"MagicLv.**0** MP.**８0**\n攻撃力**{150+(player.magic_lv()/1000)}**%の攻撃魔法 **50**%で敵を**3**ターンStun状態にする ",inline=False)
+    magic_em.add_field(name="`2.`StrengthRein",value=f"未実装",inline=False)
+    magic_em.add_field(name="`3.`IgnisStrike",value=f"未実装",inline=False)
+    magic_em.add_field(name="`4.`StrengthRein+",value=f"未実装",inline=False)
+    magic_em.add_field(name="`5.`PyrobolusLacrima",value=f"未実装",inline=False)
     magic_em.set_thumbnail(url=user.avatar_url)
     await ch.send(embed=magic_em)
 
