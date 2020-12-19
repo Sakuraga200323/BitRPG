8a8a56144f034f1fb7a8e19b4415df9be5882a83
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-20 02:23:10 +0900

    Update avatar.py

--- sub/avatar.py
+++ sub/avatar.py
@@ -68,11 +68,13 @@ class Player:
         ] = data_list
         self.max_hp = self.now_hp = self.lv_ * 100 + 10
         self.max_mp = self.now_mp = self.lv_
+        self.now_defe = self.max_defe = self.lv_ * 10 + 10 + self.defe_stp_
         magic_class = self.dtd["magic_class"]
         if magic_class == 2:
             self.max_hp = self.now_hp = int(self.max_hp*1.1)
         if magic_class == 3:
             self.max_mp = self.now_mp = int(self.max_mp*1.1)
+            self.now_defe = self.max_defe = int(self.max_defe*1.1)
         self.battle_ch = None
         self.name = str(self.user)
             
