e18afa3f4840bb4064b18acf608b0836a9952890
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-19 16:53:26 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -146,9 +146,12 @@ def create_battle_text(a,b,str_up_num=1,atk_word="攻撃",buff=0):
         if "#" in a.name: text = f"{a.name}はやられてしまった"
         else: text = f"{a.name}を倒した"
     else:
-        if "#" in a.name: no_dmg_text = f"避けられてしまった！"
-        else: no_dmg_text = f"盛大に避けた！"
-        text = f"{a.name}の{atk_word}->"
+        if "#" in a.name:
+            text = f"+ {a.name}の{atk_word}->"
+            no_dmg_text = f"避けられてしまった！"
+        else:
+            text = f"- {a.name}の{atk_word}->"
+            no_dmg_text = f"避けるなぁぁぁぁぁ！"
         if a.ID() in list((box.stun).keys())+list((box.nerf).keys()):
             if a.ID() in box.stun:
                 dmg,now_defe,now_hp = 0,b.now_defe,b.now_hp
