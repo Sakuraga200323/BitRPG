3f8facbdeafebfeb9e60366175e727a569353673
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-20 23:33:13 +0900

    Update magic_orca.py

--- sub/magic_orca.py
+++ sub/magic_orca.py
@@ -80,7 +80,7 @@ async def magic_5(player,mob):
 async def open_magic(user,ch):
     player = box.players[user.id]
     magic_em = discord.Embed(title="Player Magic Board",description="各魔法の数値は熟練度による補正を加算済みです。")
-    magic_em.add_field(name="`1.`StunRain",value=f"MagicLv.**0** MP.**８0**\n攻撃力**{150+(player.magic_lv()/1000)}**%の攻撃魔法 **50**%で敵を**3**ターンStun状態にする ",inline=False)
+    magic_em.add_field(name="`1.`StunRain",value=f"必要熟練度.**0**\n消費MP.**80**\n攻撃力**{80+(player.magic_lv()/1000)}**%の攻撃魔法 **25**%で敵に**3**ターンStun付与 ",inline=False)
     magic_em.add_field(name="`2.`StrengthRein",value=f"未実装",inline=False)
     magic_em.add_field(name="`3.`IgnisStrike",value=f"未実装",inline=False)
     magic_em.add_field(name="`4.`StrengthRein+",value=f"未実装",inline=False)
