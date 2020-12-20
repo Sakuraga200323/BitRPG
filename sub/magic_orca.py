bc298e68c3598196676a59988cac2c49bd366ef1
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-21 01:48:52 +0900

    Update magic_orca.py

--- sub/magic_orca.py
+++ sub/magic_orca.py
@@ -64,9 +64,41 @@ async def magic_1(player,mob):
     await mob.mob.send(content=magic_log)
     await battle.battle_result(player, mob)
     player.cut_mp(80)
-#  StrengthRein #
+# PalePiscis #
 async def magic_2(player,mob):
-    pass
+    ch = mob.mob
+    start_check = await battle.battle_start(player,mob)
+    if start_check is False: return
+    if player.magic_lv < 500:
+        em=discord.Embed(description="魔法練度が不足…！")
+        await ch.send(embed=em)
+        return
+    if player.now_mp < 150:
+        em=discord.Embed(description="MPが不足…！")
+        await ch.send(embed=em)
+        return
+    build_up_num = 1.1 + (player.magic_lv()/100000)
+    if mob.ID() in box.stun:
+        build_up_num += 0.5
+        del box.stun[mob.ID()]
+    if random() <= 0.5:
+        buff_num = 1
+    else:
+        buff_num = 0
+    up_num = build_up_num
+    player.magic_lv(1)
+    # 戦闘処理（Player先手） #
+    if player.AGI() >= mob.agi():
+        text1 = battle.create_battle_text(player,mob,atk_word="『PalePiscis』",str_up_num=up_num,buff=buff_num)
+        text2 = battle.create_battle_text(mob,player)
+    # 戦闘処理（Player後手） #
+    else:
+        text1 = battle.create_battle_text(mob,player)
+        text2 = battle.create_battle_text(player,mob,atk_word="『PalePiscis』",str_up_num=up_num,buff=buff_num)
+    magic_log = f"```diff\n{text1}``````diff\n{text2}```"
+    await mob.mob.send(content=magic_log)
+    await battle.battle_result(player, mob)
+    player.cut_mp(80)
 # IgnisStrike #
 async def magic_3(player,mob):
     pass
@@ -81,6 +113,7 @@ async def open_magic(user,ch):
     player = box.players[user.id]
     magic_em = discord.Embed(title="Player Magic Board",description="各魔法の数値は熟練度による補正を加算済みです。")
     magic_em.add_field(name="`1.`StunRain",value=f"必要熟練度.**0**\n消費MP.**80**\n攻撃力**{80+(player.magic_lv()/1000)}**%の攻撃魔法 **25**%で敵に**3**ターンStun付与 ",inline=False)
+    magic_em.add_field(name="`2.`PalePiscis",value=f"必要熟練度.**500**\n消費MP.**150**\n攻撃力**{110+(player.magic_lv()/1000)}**%の攻撃魔法 敵がスタン状態の時攻撃力**{160+(player.magic_lv()/1000)}**%",inline=False)
     magic_em.set_thumbnail(url=user.avatar_url)
     await ch.send(embed=magic_em)
 
