fe713b44bf7c8989e87e05395e6de1b11757eba2
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-25 00:57:29 +0900

    Update mob_data.py

--- sub/mob_data.py
+++ sub/mob_data.py
@@ -56,7 +56,7 @@ ultrarare = {
 
 
 def select(lv):
-    if random.random() <= 0.00５:
+    if random.random() <= 0.005:
         type = "UltraRare"
         result = random.choice(list(ultrarare.values()))
     if random.random() <= 0.01:
