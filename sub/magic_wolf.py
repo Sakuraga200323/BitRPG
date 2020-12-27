c9e95d4f43705f666cb13567c0129870dc432c16
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2020-12-27 14:17:25 +0900

    Update magic_wolf.py

--- sub/magic_wolf.py
+++ sub/magic_wolf.py
@@ -183,7 +183,7 @@ async def magic_5(player,mob,final=False):
     if start_check is False: return
     start_check = await battle.battle_start(player,mob)
     # 戦闘処理（Player後手） #
-    status.get_item(client.get_user(player.ID(),4,-use_num)
+    status.get_item(client.get_user(player.ID(),4,-use_num))
     text1 = battle.create_battle_text(mob,player)
     text2 = battle.create_battle_text(player,mob,atk_word=f"『{magic_name}』",str_up_num=up_num)
     battle_log = f"```diff\n{text1}```{str_up_text}```diff\n{text2}```"
