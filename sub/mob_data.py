7fc111333b484c3c32d9f5e20fb270e5b6c8dd4c
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-01 03:19:21 +0900

    Update mob_data.py

--- sub/mob_data.py
+++ sub/mob_data.py
@@ -28,15 +28,29 @@ normal = {
 'ヤバイエイ':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/04/Cthylla_2.png',
 'ヤバイイカ':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/04/Cthylla_1.png',
 'ハーピィ':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2017/05/5898684b5efafe8e5d6e7dbb4f412f06.png',
-
+'女戦士':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2018/06/6a20182725ab8a3fdeceb14b452bb44f.png',
+'女戦士':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2018/06/9101751550bbbbae4878a004e2022460.png',
+'スプリガン':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2016/08/a705c64f123ea0621f687f2d77fca782.png',
+'ドワーフ':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/08/90a640ca37cff2b8acd41fd0b78e92df.png',
+'大地の精霊':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/07/410bea672cb680e671eec8f6c76e1c09.png',
+'森の精霊':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/06/f8424ad1e7fb5a361a299709efdae9d7.png',
+'火の精霊':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/06/3a76d6930d86df706d47cdfcd3b03596.png',
+'雷の精霊':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/06/4b5d5fb3c4569234ddb2657438a74271.png',
+'ガーゴイル':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/06/63e8fef26f6dc883380f7ca1876b127f.png',
+'天使':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2011/07/angel_11.png',
+'黒塚':http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/04/kurotsuka_1.png',
+'ドラゴノイド':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/03/dragonoid_r.png',
+'騎士':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2010/12/armor_b.png',
+'ミートホープ':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2010/12/kotori.png'
 }
 
 elite = {
+'侵略-聖母マリア':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/06/e181b8bc72c5ae7d98541fc9b10370b9.png',
 'ダークグール':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/06/%E3%82%B0%E3%83%BC%E3%83%AB_2.png',
 'ヴァルキリー':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2020/12/497ee39d4e650c5d5e4033c677c7926a.png',
 'レインボースライム':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2017/05/c211c0e4782fdef1fb6a9014ce9e31af.png',
 'オークライダー':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2019/10/f24cb922ca3de847ddb2a8a5ba5ccd4e.png',
-'覚醒格闘家-龍姫':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2019/03/a77c93f5b8cb49ca86b756b02a7342ff.png',
+'龍格闘家-龍姫':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2019/03/a77c93f5b8cb49ca86b756b02a7342ff.png',
 '水神-アマビエ':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2020/03/b23125c46c37fecb018cfa25121d4094.png',
 'ラミエル':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2017/06/8e13afa0e6256e72d755c60d0afe4bb4.png',
 'ラミエル':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2017/06/8e704e898fd57a2f6ce0e0aa86de9c3b.png',
@@ -45,15 +59,22 @@ elite = {
 '三女神ノ一柱':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2020/12/2b6756d998233192a490ab0bf87f391a.png',
 '三女神ノ一柱':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2020/12/bcd1c21445c0f66532a6909a28a7417b.png',
 'クトゥルフ実娘-クティーラ':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/04/Cthylla_3L.png',
+'エグリゴリ・トゥリエル':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2014/06/%E3%83%88%E3%82%A5%E3%83%AA%E3%82%A8%E3%83%AB.png',
+'堕天使-バレンタイン':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2012/01/valentine_s.png',
+'覚醒-ドラゴノイド':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/03/dragonoid_ultimet_r.png',
 }
 
 catastrophe = {
-    1:("Catastrophe","https://blog-imgs-29-origin.fc2.com/e/u/r/eurs/m284b.png")
-}
-  
-worldend = {
 '三女神':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2020/12/2da57be77b70a9eaca606109dfc41822.png',
+'覚醒龍格闘家-龍姫':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2011/10/ryuho_ki_L_efect.png',
+'超覚醒-ドラゴノイド':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/03/dragonoid_infinity_r.png',
+'デュラハン':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2010/12/armor_d.png',
+'氷の女王-シヴァ':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2012/03/shiva_l.png'
+}
+
+worldend = {
 'プレイシュヴァ':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/02/breasvha.png',
+'真理':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2010/12/abronxless_2.png'
 }
 
 rare = {
@@ -63,7 +84,7 @@ rare = {
 'スターウィザード':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2019/01/ea470cf0eb57c3fd397c76dd8033db03.png',
 'ゴールド-モノリス':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2017/06/e591677f3dcebe6d5f001844a7ccb877.png',
 'モロナイ':'http://darts-x.sakura.ne.jp/m/wp-content/uploads/2018/06/574126928be7084ff3cf07433a1f2b17.png',
-
+'サキュバス':'http://darts-x.sakura.ne.jp/m//wp-content/uploads/2011/03/Succubus_1.png'
 }
 
 ultrarare = {
@@ -86,6 +107,10 @@ def select(lv):
         type = "WorldEnd"
         name = random.choice(list(worldend.keys()))
         img = worldend[name]
+    elif lv % 100 == 0:
+        type = "Elite"
+        name = random.choice(list(catastrophe.keys()))
+        img = catastrophe[name]
     elif lv % 10 == 0:
         type = "Elite"
         name = random.choice(list(elite.keys()))
