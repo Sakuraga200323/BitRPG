90e8725e54d34c47d0a955559a8e9f2a1faa2ed4
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-09 12:59:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -185,6 +185,8 @@ async def loop():
     sub_msg = '^^start'
     if client.get_channel(761571389345759232).name=='true':
         sub_msg = "開発作業中"
+    if not pg or not pg2:
+        sub_msg = "データベース問題発生"
     await client.change_presence(activity=discord.Game(name=f"{sub_msg}｜{len(client.guilds)}server"))
     if log_text != "":
         log_ch = client.get_channel(791128460726501378)
