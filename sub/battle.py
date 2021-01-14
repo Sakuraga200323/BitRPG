31ccf19e1b236bf32c849f992fba8c629f49ef35
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-14 11:07:56 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -293,8 +293,9 @@ async def reset(user, ch):
     if mob.ID() in box.nerf:
         del box.nerf[mob.ID()]
     await ch.send(embed = mob.spawn())
-    if mob.type in ("Elite","UltraRare",""):
-        box.anti_magic.append(mob.ID())
+    if mob.type in ("Elite","UltraRare","Catastrophe"):
+        if not mob.ID() in ant_magic:
+           box.anti_magic.append(mob.ID())
         anti_magic_em = discord.Embed(description=f"{mob.name}のアンチマジックエリアが発動！")
         await ch.send(embed=anti_magic_em)
                     
