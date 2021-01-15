5be0456acf976b897908d737858498af70a4b976
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-15 17:46:42 +0900

    Update shop.py

--- sub/shop.py
+++ sub/shop.py
@@ -71,15 +71,19 @@ items_emoji_a = {
 
 
 async def shop(user, ch):
+    # shop_msg1 -> ショプの最初のメッセージ
+    # shop_msg2 -> コマンドの使いかたや状況の詳細を書き込むメッセージ
     player = box.players[user.id]
-    shop_em = discord.Embed(
+    shop_em1 = discord.Embed(
         title="Shop",
-        description=("`該当するサービスの番号を半角英数字で送信してください。`"
-            + "\n`1.`アイテム購入"
+        description=(
+            "\n`1.`アイテム購入"
             + "\n`2.`アイテム合成"
             + "\n`3.`~~武器購入~~"
     ))
-    shop_em_msg = await ch.send(embed=shop_em)
+    shop_em2 = discord.Embed(description="`該当するサービスの番号を半角英数字で送信してください。`")
+    shop_msg1 = await ch.send(embed=shop_em1)
+    shop_msg2 = await ch.send(embed=shop_em2)
     def check(m):
         if not user.id == m.author.id:
             return 0
@@ -91,14 +95,14 @@ async def shop(user, ch):
     try:
         msg = await client.wait_for("message", timeout=60, check=check)
     except asyncio.TimeoutError:
-        em = discord.Embed(description=f"冷やかしはお断りだよ！")
-        await ch.send(embed=em)
+        em = discord.Embed(description=f"指定がないので処理を終了しました")
+        await shop_msg2.edit(embed=em)
     else:
         respons = int(msg.content) if msg.content in ("1","2","3",) else 0
         if respons == 1:
             service_em1 = discord.Embed(
                 title="アイテムショップ",
-                description=("`該当するアイテムの番号と購入数を半角英数字で送信してください。\n例(HP回復薬を10個購入)『1 10』`"
+                description=(f"所持Cell{player.money()}"
                     + f"\n`1.`{items_emoji_a[2] }`HP回復薬　`[100cell]\n`Info: HPを30%回復`"
                     + f"\n`2.`{items_emoji_a[3] }`MP回復薬　`[100cell]\n`Info: MPを30%回復`"
                     + f"\n`3.`{items_emoji_a[4] }`魂の焔　  `[10cell]\n`Info: 素材アイテム`"
@@ -109,40 +113,44 @@ async def shop(user, ch):
                     + f"\n`8.`{items_emoji_a[9] }`HP全回復薬`[300cell]\n`Info: HPを100%回復`"
                     + f"\n`9.`{items_emoji_a[10]}`MP全回復薬`[300cell]\n`Info: MPを100%回復`"
             ))
-            await shop_em_msg.edit(embed=service_em1)
+            em = discord.Embed(description="`該当するアイテムの番号と購入数を半角スペースを空けて送信してください。\n例(HP回復薬を10個購入)1 10`")
+            await shop_msg1.edit(embed=service_em1)
+            await shop_msg2.edit(embed=em)
             try:
                 msg = await client.wait_for("message", timeout=60, check=check)
             except asyncio.TimeoutError:
-                em = discord.Embed(description=f"冷やかしはお断りだよ、出直してきな！")
-                await ch.send(embed=em)
+                em = discord.Embed(description=f"指定がないので処理を終了しました")
+                await shop_msg2.edit(embed=em)
             else:
                 pattern = r'^(\d+) (\d+)$'
                 result = re.search(pattern, msg.content)
                 if not result:
-                    em = discord.Embed(description=f"ちゃんと注文して")
-                    await ch.send(embed=em)
+                    em = discord.Embed(description=f"注文の仕方が違います `購入するアイテムの番号 購入数` です")
+                    await shop_msg2.edit(embed=em)
                     return
                 item_id, item_num = int(result.group(1))+1, int(result.group(2))
                 cost_dict = {2:100,3:100,4:10,5:500,6:150,7:1000,8:2000,9:300,10:300}
                 if player.money() < cost_dict[item_id]*item_num:
                     em = discord.Embed(description=f"{cost_dict[item_id]*item_num-player.money()}cell程お金が足りないようです。")
-                    await ch.send(embed=em)
+                    await shop_msg2.edit(embed=em)
                     return
                 status.get_item(user,item_id,item_num)
                 player.money(-cost_dict[item_id]*item_num)
                 result_em = discord.Embed(description=f"{cost_dict[item_id]*item_num}cellで{items_name[item_id]}{items_emoji_a[item_id]}x{item_num}を購入しました。\nまたのご来店をお待ちしております！")
-                await ch.send(embed=result_em)
+                await shop_msg2.edit(embed=result_em)
 
         elif respons == 2:
             service_em2 = discord.Embed(
                 title="合成場",
-                description=("`該当するアイテムの番号と合成数を半角英数字で送信してください。\n例(HP回復薬を10個購入)『1 10』`"
+                description=(f"所持Cell{player.money()}"
                     + f"\n`1.`{items_emoji_a[7] }`魔　晶  　`[500cell {items_emoji_a[5]}×1 {items_emoji_a[6]}×1]\n`Info: 素材アイテム`"
                     + f"\n`2.`{items_emoji_a[8] }`魔硬貨  　`[750cell {items_emoji_a[4]}×1 {items_emoji_a[5]}×1 {items_emoji_a[7]}×1]\n`Info: とある魔法の触媒`"
                     + f"\n`3.`{items_emoji_a[9] }`HP全回復薬`[200cell {items_emoji_a[2]}×1 {items_emoji_a[4]}×10]\n`Info: HPを100%回復`"
                     + f"\n`4.`{items_emoji_a[10]}`MP全回復薬`[200cell {items_emoji_a[3]}×1 {items_emoji_a[4]}×10]\n`Info: MPを100%回復`"
             ))
-            await shop_em_msg.edit(embed=service_em2)
+            em = discord.Embed(description="`該当するアイテムの番号と合成数を半角英数字で送信してください。\n例(HP回復薬を10個合成)1 10`")
+            await shop_msg1.edit(embed=service_em2)
+            await shop_msg2.edit(embed=em)
             try:
                 msg = await client.wait_for("message", timeout=60, check=check)
             except asyncio.TimeoutError:
@@ -152,8 +160,8 @@ async def shop(user, ch):
                 pattern = r'^(\d+) (\d+)$'
                 result = re.search(pattern, msg.content)
                 if not result:
-                    em = discord.Embed(description=f"ちゃんと注文して")
-                    await ch.send(embed=em)
+                    em = discord.Embed(description=f"注文の仕方が違います `合成するアイテムの番号 購入数` です")
+                    await shop_msg2.edit(embed=em)
                     return
                 item_id, item_num = int(result.group(1))+6, int(result.group(2))
                 item_name = items_name[item_id]
@@ -172,12 +180,12 @@ async def shop(user, ch):
                         husoku_text += f"{i_name}{items_emoji_a[data[0]]}×{data[1]*item_num-item_dtd[i_name]} "
                         continue
                 if husoku_text != "":
-                    em = discord.Embed(description=f"{husoku_text}が足りないです。")
-                    await ch.send(embed=em)
+                    em = discord.Embed(description=f"{husoku_text}が足りないです")
+                    await shop_msg2.edit(embed=em)
                     return
                 if player.money() < cost_dict[item_id]*item_num:
-                    em = discord.Embed(description=f"{cost_dict[item_id]*item_num-player.money()}cell程お金が足りないようです。")
-                    await ch.send(embed=em)
+                    em = discord.Embed(description=f"{cost_dict[item_id]*item_num-player.money()}cell程お金が足りないようです")
+                    await shop_msg2.edit(embed=em)
                     return
                 for data in material_dict[item_id]:
                     i_name = items_name[data[0]]
@@ -185,7 +193,7 @@ async def shop(user, ch):
                 status.get_item(user,item_id,item_num)
                 player.money(-cost_dict[item_id]*item_num)
                 em = discord.Embed(description=f"{cost_dict[item_id]*item_num}cellで{item_name}{items_emoji_a[item_id]}x{item_num}を合成しました。\nまたのご来店をお待ちしております！")
-                await ch.send(embed=em)
+                await shop_msg2.edit(embed=em)
 
 
 
@@ -205,26 +213,26 @@ async def shop(user, ch):
             embeds = tuple(embeds)
             desc_first_text = "`対応するローマ字を送信で購入\n数字を送信するとページ切り替え\nRankは購入時にランダムで決められるランクの最大値です`"
             first_em = discord.Embed(description=desc_first_text)
-            first_em_msg = await ch.send(embed=first_em)
+            await shop_msg1.edit(embed=first_em)
             page_num = 1
             while client:
-                await shop_em_msg.edit(embed=embeds[page_num-1])
+                await shop_msg1.edit(embed=embeds[page_num-1])
                 try:
                     weapon_shop_msg = await client.wait_for("message", timeout=20, check=check)
                 except asyncio.TimeoutError:
                     em = discord.Embed(description=f"指定がないので処理終了しました")
-                    await first_em_msg.edit(embed=em)
+                    await shop_msg2.edit(embed=em)
                     break
                 else:
                     page_num  = int(weapon_shop_msg.content)
                     if page_num == 0:
                         await weapon_shop_msg.delete()
                         em = discord.Embed(description=f"処理を終了しました")
-                        await first_em_msg.edit(embed=em)
+                        await shop_msg2.edit(embed=em)
                         break
                     if 0 < respons < len(embeds):
                         em = embeds[respons-1]
-                        await shop_em_msg.edit(embed=embeds[page_num-1])
+                        await shop_msg1.edit(embed=embeds[page_num-1])
                     
 
 
