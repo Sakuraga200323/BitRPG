3831aec3bc598a01a503b557930431caca66f80d
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-15 22:01:58 +0900

    Update battle.py

--- sub/battle.py
+++ sub/battle.py
@@ -118,7 +118,7 @@ async def battle_result(player, mob):
         if  now in ['23:18']:
             exp *= 16
             await ch.send("????『幸運を。死したものより祝福を。』")
-        print(f"『{mob.name　<15}』(Lv.{mob.lv})を{[ client.get_user(i) for i in mob.battle_players]}が討伐")
+        print(f"『{mob.name:　<15}』(Lv.{mob.lv})を{[ client.get_user(i) for i in mob.battle_players]}が討伐")
         for p_id in mob.battle_players:
             p = box.players[p_id]
             EXP = exp
