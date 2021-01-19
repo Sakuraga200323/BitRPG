47bf40bd59c254482ba78bffba5c68f635a8e7c0
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-19 21:17:55 +0900

    Update shop.py

--- sub/shop.py
+++ sub/shop.py
@@ -185,8 +185,8 @@ async def shop(user, ch):
                         continue
                     for data in material_dict[item_id]:
                         i_name = items_name[data[0]]
-                        player.get_item(user,data[0],-data[1]*item_num)
-                    player.get_item(user,item_id,item_num)
+                        player.get_item(data[0],-data[1]*item_num)
+                    player.get_item(item_id,item_num)
                     player.money(-cost_dict[item_id]*item_num)
                     shop_em2.description = f"所持Cell:{player.money()}"
                     await shop_msg.edit(
