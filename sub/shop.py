ae24795f7ce947b92e74d383e759252a5d6b4d6a
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-20 09:20:34 +0900

    Update shop.py

--- sub/shop.py
+++ sub/shop.py
@@ -278,7 +278,7 @@ async def shop(user, ch):
                                 )
                                 break
                             weapon_id = int(msg.content) + (page_num)*6
-                            weapon = npc_weapons[weapon_id]
+                            weapon = box.npc_weapons[weapon_id]
                             if player.weapon_id >= 5:
                                 await shop_msg.edit(
                                     content=f"```既に５個の武器を所持しています。```",
