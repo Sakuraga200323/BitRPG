5e4cad392a4df04bf4807989bd3390358e22a525
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-21 16:23:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -131,16 +131,16 @@ signal.signal(signal.SIGTERM, handler)
 async def on_ready():
     await client.change_presence(activity=discord.Game(name=f"起動中…"))
     players_id = [ i["id"] for i in pg.fetchdict("select id from player_tb order by lv desc;")]
+    for weapon_id in weapons_id:
+        weapon = avatar.Weapon(weapon_id)
+        box.weapons[weapon_id] = weapon
+        print("Weaponデータ挿入： ", weapon.name, f"Rank.{weapon.rank()} Lv.{weapon.lv()}")
     for player_id in players_id:
         if client.get_user(player_id):
             player = avatar.Player(client, player_id)
             box.players[player_id] = player
             print("Playerデータ挿入： ",player.user)
     weapons_id = [ i["id"] for i in pg.fetchdict("select id from weapon_tb order by lv desc;")]
-    for weapon_id in weapons_id:
-        weapon = avatar.Weapon(weapon_id)
-        box.weapons[weapon_id] = weapon
-        print("Weaponデータ挿入： ", weapon.name, f"Rank.{weapon.rank()} Lv.{weapon.lv()}")
     p_num_result = (len(player_ids)==len(box.players))
     desc = (
           f"\n+Prefix『^^』"
