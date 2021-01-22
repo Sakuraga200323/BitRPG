a5602ed879703fc5141488116bf9fce545bcc258
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-22 13:25:21 +0900

    Update status.py

--- sub/status.py
+++ sub/status.py
@@ -506,17 +506,19 @@ async def set_weapon(user,ch):
                     else:
                         num = int(msg.content)
                         if num == 0:
-                            await menu_msg.edit(content="```処理終了済み```")
+                            await msg0.edit(content="```処理終了済み```")
                             break
                         else:
                             weapon = weapons_num[num-1]
                             player.drop_weapon(weapon=weapon)
                             em = discord.Embed(title="Drop Weapon")
+                            num = 0
                             for weapon in player.weapons():
                                 if player.weapon() and weapon.id == player.weapon().id:
                                     em.add_field(name=f"▷{weapon.emoji()}{weapon.name()}",value=f"`Rank.{weapon.rank()}┃Lv.{weapon.lv()}┃Atk.{weapon.strength()}`",inline=False)
                                 else:
-                                    em.add_field(name=f"{weapon.emoji()}`{weapon.name()}`",value=f"`Rank.{weapon.rank()}┃Lv.{weapon.lv()}┃Atk.{weapon.strength()}`",inline=False)
-                                weapons_num.append(weapon)
+                                    num += 1
+                                    em.add_field(name=f"`{num}.`{weapon.emoji()}`{weapon.name()}`",value=f"`Rank.{weapon.rank()}┃Lv.{weapon.lv()}┃Atk.{weapon.strength()}`",inline=False)
+                                    weapons_num.append(weapon)
                             await msg0.edit(content="```消去完了。\n引き続き消去出来ます。\n終了する場合は0を送信。```",embed=em)
             
