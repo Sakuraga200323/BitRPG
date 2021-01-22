b27de799dcd4b0f868defad263002c6a522f5ad2
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-01-22 20:26:07 +0900

    Update shop.py

--- sub/shop.py
+++ sub/shop.py
@@ -111,7 +111,7 @@ async def shop(user, ch):
                         )
                         break
                     item_id_dict = {1:2,2:3,3:4,4:5,5:6,6:7,7:8,8:9,9:10,10:26,11:27}
-                    if not result.group(1) in item_id_dict:
+                    if not int(result.group(1)) in item_id_dict:
                         await shop_msg.edit(
                             content="```番号オーバーしてるかもです```",
                         )
