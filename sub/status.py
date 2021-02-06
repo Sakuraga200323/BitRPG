8ab38e732156e728e832902f69e96477d3824573
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-02-06 12:10:13 +0900

    Update status.py

--- sub/status.py
+++ sub/status.py
@@ -539,10 +539,10 @@ async def set_weapon(user,ch):
                     buildup_em = discord.Embed(title="Buildup Weapon")
                     buildup_em.add_field(name='強化武器',value=f"{target_weapon_obj.emoji()}{target_weapon_obj.name()}")
                     material_text = ""
-                    for i in zip((1,2,3,4),(29,30):
+                    for item_num,i in zip((1,2,3,4),(29,30)):
                         num = player.item_num(i)
                         if num:
-                            material_text += f"{box.items_emoji[i]}×{num} "
+                            material_text += f"{item_num}{box.items_emoji[i]}×{num} "
                     if material_text == "":
                         material_text = "強化素材がありません"
                     buildup_em.add_field(name=f"強化素材",value=material_text)
